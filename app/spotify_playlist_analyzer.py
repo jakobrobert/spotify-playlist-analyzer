0b80f6670fbf46cbfa8e0a64856297568813da89
jakobrobert <jakob-robert@hotmail.de>
2022-02-05 14:37:00 +0100

    Add textfield where user can enter URL of playlist and a button to send request.

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 import configparser
 
@@ -13,3 +12,12 @@ app = Flask(__name__)
 @app.route(URL_PREFIX, methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route(URL_PREFIX + "get-songs-of-playlist", methods=["GET"])
+def get_songs_of_playlist():
+    playlist_url = request.args.get("playlist_url")
+    print(f"playlist_url: {playlist_url}")
+
+    # TODO get data of this playlist using Spotify Web API
+    return "TODO"
