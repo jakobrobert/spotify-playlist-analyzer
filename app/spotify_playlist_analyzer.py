7f6bcfc45436d7b9a0ebe5e65c50cc887deaef3f
jakobrobert <jakob-robert@hotmail.de>
2022-02-05 14:01:37 +0100

    Read url prefix from .ini file to be flexible for multiple deployments

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -1,8 +1,14 @@
 from flask import Flask
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read("../server.ini")
+URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
+
 app = Flask(__name__)
 
 
-@app.route("/spotify-playlist-analyzer/dev/")
+@app.route(URL_PREFIX, methods=["GET"])
 def index():
     return "<p>Hello, World!</p>"
