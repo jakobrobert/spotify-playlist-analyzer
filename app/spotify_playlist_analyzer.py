f2cfce4cd45ae3e1aea5e0823cb64cdf724be23f
jakobrobert <jakob-robert@hotmail.de>
2022-02-06 19:14:53 +0100

    Show actual data of songs

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -18,29 +18,39 @@ def index():
 @app.route(URL_PREFIX + "songs-of-playlist", methods=["GET"])
 def get_songs_of_playlist():
     playlist_url = request.args.get("playlist_url")
-    print(f"playlist_url: {playlist_url}")
+    #print(f"playlist_url: {playlist_url}")
 
     # TODO this is just a test token, replace by proper Authentication flow
     access_token = "BQBPTOQbOcNgbylEde97_eMLX4AilcQxnEMP0WuaDvmex_5CeGUW3IwCECcQ4xCtze3xsgdh9UPyEMzu5u9XxXkweHTTm1wjBTd97IGQBJa8zIBdEpbU3N7hVU2ZQ2Z8Mha0JSCu40YRUrveFb-ScFXn17KeWyjbgnI "
     playlist_id = "2cE4QPx8FAtUIOsZpHwAQM"  # TODO currently hardcoded, get playlist id based on the playlist url
     url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
 
-    response = requests.get(
-        url,
+    response = requests.get(url,
         headers={
             "Authorization": f"Bearer {access_token}"
         }
     )
-    response_json = response.json()
-    print(f"response_json: {response_json}")
+
+    response_data = response.json()
+    tracks = response_data["tracks"]["items"]
 
     songs = []
 
-    # TODO use data of response
-    """
-    for i in range(0, 10):
-        song = {"artist": f"Artist {i}", "title": f"Title {i}"}
+    # TODO this contains only 100 songs, even though playlist has more songs. need to set limit param or send several requests?
+    for track in tracks:
+        track_item = track["track"]
+
+        title = track_item["name"]
+
+        artists = track_item["artists"]
+        artists_string = ""
+        for i in range(len(artists)):
+            artist_name = artists[i]["name"]
+            if i != 0:
+                artists_string += ", "
+            artists_string += artist_name
+
+        song = {"artists": artists_string, "title": title}
         songs.append(song)
-    """
 
     return render_template("songs_of_playlist.html", songs=songs, num_songs=len(songs))
