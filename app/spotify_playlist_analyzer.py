78c6ed9c19a6d9622803b0ff60cf04e3dd79b8da
jakobrobert <jakob-robert@hotmail.de>
2022-02-06 18:43:15 +0100

    Get data of playlist using Spotify Web API. Currently for a hardcoded playlist_id.

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 
 import configparser
+import requests
 
 config = configparser.ConfigParser()
 config.read("../server.ini")
@@ -19,11 +20,27 @@ def get_songs_of_playlist():
     playlist_url = request.args.get("playlist_url")
     print(f"playlist_url: {playlist_url}")
 
-    # TODO get data of this playlist using Spotify Web API
+    # TODO this is just a test token, replace by proper Authentication flow
+    access_token = "BQBPTOQbOcNgbylEde97_eMLX4AilcQxnEMP0WuaDvmex_5CeGUW3IwCECcQ4xCtze3xsgdh9UPyEMzu5u9XxXkweHTTm1wjBTd97IGQBJa8zIBdEpbU3N7hVU2ZQ2Z8Mha0JSCu40YRUrveFb-ScFXn17KeWyjbgnI "
+    playlist_id = "2cE4QPx8FAtUIOsZpHwAQM"  # TODO currently hardcoded, get playlist id based on the playlist url
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+
+    response = requests.get(
+        url,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+    response_json = response.json()
+    print(f"response_json: {response_json}")
 
     songs = []
+
+    # TODO use data of response
+    """
     for i in range(0, 10):
         song = {"artist": f"Artist {i}", "title": f"Title {i}"}
         songs.append(song)
+    """
 
     return render_template("songs_of_playlist.html", songs=songs, num_songs=len(songs))
