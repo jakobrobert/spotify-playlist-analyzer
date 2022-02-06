708d7fb6608df1c5e5d6a9e02acded388aa7c00d
jakobrobert <jakob-robert@hotmail.de>
2022-02-06 19:35:18 +0100

    #2 Show list of songs of playlist (#6)
    * Add textfield where user can enter URL of playlist and a button to send request.
    
    * Add template to show songs of playlist, use dummy data.
    
    * Show number of songs
    
    * Get data of playlist using Spotify Web API. Currently for a hardcoded playlist_id.
    
    * Show actual data of songs
    
    * Get playlist_id from the playlist_url instead of hardcoding it
    
    * Remove TODOS, replaced them by GitHub issues

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 import configparser
+import requests
 
 config = configparser.ConfigParser()
 config.read("../server.ini")
@@ -13,3 +13,44 @@ app = Flask(__name__)
 @app.route(URL_PREFIX, methods=["GET"])
 def index():
     return render_template("index.html")
+
+
+@app.route(URL_PREFIX + "songs-of-playlist", methods=["GET"])
+def get_songs_of_playlist():
+    playlist_url = request.args.get("playlist_url")
+
+    playlist_id_start_index = playlist_url.find("playlist/") + len("playlist/")
+    playlist_id_end_index = playlist_url.find("?")
+    playlist_id = playlist_url[playlist_id_start_index:playlist_id_end_index]
+
+    access_token = "BQBPTOQbOcNgbylEde97_eMLX4AilcQxnEMP0WuaDvmex_5CeGUW3IwCECcQ4xCtze3xsgdh9UPyEMzu5u9XxXkweHTTm1wjBTd97IGQBJa8zIBdEpbU3N7hVU2ZQ2Z8Mha0JSCu40YRUrveFb-ScFXn17KeWyjbgnI "
+    url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+
+    response = requests.get(url,
+        headers={
+            "Authorization": f"Bearer {access_token}"
+        }
+    )
+
+    response_data = response.json()
+    tracks = response_data["tracks"]["items"]
+
+    songs = []
+
+    for track in tracks:
+        track_item = track["track"]
+
+        title = track_item["name"]
+
+        artists = track_item["artists"]
+        artists_string = ""
+        for i in range(len(artists)):
+            artist_name = artists[i]["name"]
+            if i != 0:
+                artists_string += ", "
+            artists_string += artist_name
+
+        song = {"artists": artists_string, "title": title}
+        songs.append(song)
+
+    return render_template("songs_of_playlist.html", songs=songs, num_songs=len(songs))
