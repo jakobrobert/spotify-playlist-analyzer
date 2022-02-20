fab84722be4f07e617541dd1362c3b41dae629a0
jakobrobert <jakob-robert@hotmail.de>
2022-02-20 20:05:58 +0100

    #9 Show more song data (#14)
    * Add column for duration, extract function __get_artists_of_track
    
    * Format duration as minutes:seconds
    
    * Add column for release date

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 
 import configparser
 import requests
+import datetime
 
 config = configparser.ConfigParser()
 config.read("../server.ini")
@@ -35,19 +36,21 @@ def get_songs_of_playlist():
         track_item = track["track"]
 
         title = track_item["name"]
+        artists = __get_artists_of_track(track_item)
+        duration = __get_duration_of_track(track_item)
+        release_date = __get_release_date_of_track(track_item)
 
-        artists = track_item["artists"]
-        artists_string = ""
-        for i in range(len(artists)):
-            artist_name = artists[i]["name"]
-            if i != 0:
-                artists_string += ", "
-            artists_string += artist_name
-
-        song = {"artists": artists_string, "title": title}
+        song = {"artists": artists, "title": title, "duration": duration, "release_date": release_date}
         songs.append(song)
 
     return render_template("songs_of_playlist.html", songs=songs, num_songs=len(songs))
+
+
+def __get_playlist_id_from_playlist_url(playlist_url):
+    start_index = playlist_url.find("playlist/") + len("playlist/")
+    end_index = playlist_url.find("?")
+
+    return playlist_url[start_index:end_index]
 
 
 def __get_spotify_access_token():
@@ -62,9 +65,31 @@ def __get_spotify_access_token():
     return response_data["access_token"]
 
 
-def __get_playlist_id_from_playlist_url(playlist_url):
-    start_index = playlist_url.find("playlist/") + len("playlist/")
-    end_index = playlist_url.find("?")
+def __get_artists_of_track(track):
+    artists_string = ""
+    artists = track["artists"]
 
-    return playlist_url[start_index:end_index]
+    for i in range(len(artists)):
+        artist_name = artists[i]["name"]
+        if i != 0:
+            artists_string += ", "
+        artists_string += artist_name
+
+    return artists_string
+
+
+def __get_duration_of_track(track):
+    milliseconds = track["duration_ms"]
+    total_seconds = milliseconds // 1000
+    total_minutes = total_seconds // 60
+    remaining_seconds = total_seconds % 60
+
+    return f"{total_minutes:02d}:{remaining_seconds:02d}"
+
+
+def __get_release_date_of_track(track):
+    album = track["album"]
+    release_date = album["release_date"]
+
+    return release_date
 
