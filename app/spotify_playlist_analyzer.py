9ad433b7f6b3e005d6ae71592a7264e83cb11b13
jakobrobert <jakob-robert@hotmail.de>
2022-03-06 19:02:41 +0100

    Implement sort functionality: Works fine (even though not really efficient, might optimize it later)

--- app/spotify_playlist_analyzer.py
+++ app/spotify_playlist_analyzer.py
@@ -29,6 +29,12 @@ def get_songs_of_playlist():
     songs = spotify_client.get_songs_of_playlist(playlist_id)
 
     # TODO sort if params defined and not value "none"
+    sort_by = request.args.get("sort_by")
+    print(f"sort_by: {sort_by}")
+    ascending_or_descending = request.args.get("ascending_or_descending")
+    print(f"ascending_or_descending: {ascending_or_descending}")
+
+    __sort_songs(songs, sort_by, ascending_or_descending)
 
     # TODO clean up: num_songs is obsolete, can use len(songs) in jinja code?
     return render_template("songs_of_playlist.html", songs=songs, num_songs=len(songs), playlist_url=playlist_url)
@@ -39,3 +45,15 @@ def __get_playlist_id_from_playlist_url(playlist_url):
     end_index = playlist_url.find("?")
 
     return playlist_url[start_index:end_index]
+
+
+def __sort_songs(songs, sort_by, ascending_or_descending):
+    if sort_by is None or sort_by == "none":
+        return
+
+    if ascending_or_descending is None or ascending_or_descending == "none":
+        return
+
+    reverse = (ascending_or_descending == "descending")
+    print(f"songs[0][sort_by]: {songs[0][sort_by]}")
+    songs.sort(key=lambda song: song[sort_by], reverse=reverse)
