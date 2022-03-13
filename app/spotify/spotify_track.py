376fe337634fba78c3743c9c5af0f55f166e02af
jakobrobert <jakob-robert@hotmail.de>
2022-03-13 18:42:01 +0100

    Refactor: Move artist_ids out of SpotifyTrack class because is only needed temporarily to get other data, does not belong to actual model representation

--- app/spotify/spotify_track.py
+++ app/spotify/spotify_track.py
@@ -3,9 +3,8 @@ class SpotifyTrack:
         self.track_id = ""
         self.title = "n/a"
         self.artists = "n/a"   # TODO change to array of strings
-        self.duration = "n/a"  # TODO change to TimeSpan
+        self.duration = "n/a"
         self.year_of_release = -1
-        self.artist_ids = []
         self.genres = "n/a"    # TODO change to array of strings
         self.tempo = -1.0
         self.key = "n/a"
