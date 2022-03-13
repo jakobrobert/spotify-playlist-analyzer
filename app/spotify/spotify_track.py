862bbc4cde64e28213944a0f7be47bd3522ad003
jakobrobert <jakob-robert@hotmail.de>
2022-03-13 19:01:08 +0100

    #35 Extract class SpotifyTrack (#44)
    * Add class SpotifyTrack
    
    * Use class SpotifyTrack. TODO: Now TypeError in sort function, need to fix.
    
    * Fix sort function
    
    * Clean up: Rename variables
    
    * Refactor: Move artist_ids out of SpotifyTrack class because is only needed temporarily to get other data, does not belong to actual model representation
    
    * Refactor: Move track_id out of SpotifyTrack class for same reason
    
    * Store artists and genres as array of strings instead of one formatted string
    
    * Format genres and artists in template code
    
    * Extract method __set_genres_of_tracks

---
+++ app/spotify/spotify_track.py
@@ -0,0 +1,11 @@
+class SpotifyTrack:
+    def __init__(self):
+        self.title = "n/a"
+        self.artists = []
+        self.duration = "n/a"
+        self.year_of_release = -1
+        self.genres = []
+        self.tempo = -1.0
+        self.key = "n/a"
+        self.mode = "n/a"
+        self.loudness = -1.0
