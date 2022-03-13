545d9587694e4882e7bc22e713449a200c7270bd
jakobrobert <jakob-robert@hotmail.de>
2022-03-13 18:56:07 +0100

    Format genres and artists in template code

--- app/spotify/spotify_track.py
+++ app/spotify/spotify_track.py
@@ -1,10 +1,10 @@
 class SpotifyTrack:
     def __init__(self):
         self.title = "n/a"
-        self.artists = "n/a"   # TODO change to array of strings
+        self.artists = []
         self.duration = "n/a"
         self.year_of_release = -1
-        self.genres = "n/a"    # TODO change to array of strings
+        self.genres = []
         self.tempo = -1.0
         self.key = "n/a"
         self.mode = "n/a"
