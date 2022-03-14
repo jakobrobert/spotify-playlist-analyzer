4c88a42ca92eb2e102c013cbec1be289b2470eec
jakobrobert <jakob-robert@hotmail.de>
2022-03-14 20:38:01 +0100

    Clean up code

--- app/spotify/spotify_client.py
+++ app/spotify/spotify_client.py
@@ -13,27 +13,18 @@ class SpotifyClient:
         self.CLIENT_ID = client_id
         self.CLIENT_SECRET = client_secret
 
-    def get_name_of_playlist(self, playlist_id):
-        url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
-        access_token = self.__get_access_token()
-        headers = {"Authorization": f"Bearer {access_token}"}
-        response = requests.get(url, headers=headers)
-        response_data = response.json()
-
-        return response_data["name"]
-
     def get_playlist_by_id(self, playlist_id):
         url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
         access_token = self.__get_access_token()
         headers = {"Authorization": f"Bearer {access_token}"}
         response = requests.get(url, headers=headers)
-        response_data = response.json() # TODO rename to playlist_data
+        playlist_data = response.json()
 
         playlist = SpotifyPlaylist()
-        playlist.name = response_data["name"]
+        playlist.name = playlist_data["name"]
 
         # TODO extract method get_tracks_of_playlist(response_data)
-        track_items = response_data["tracks"]["items"]
+        track_items = playlist_data["tracks"]["items"]
 
         tracks = []
         all_artist_ids = []
