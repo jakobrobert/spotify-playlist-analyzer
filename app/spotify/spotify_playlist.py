62fd080d13418e90a75ebe3a4cb09f7973f55fda
jakobrobert <jakob-robert@hotmail.de>
2022-03-16 21:28:42 +0100

    Show total duration of playlist. In milliseconds, also change duration in SpotifyTrack to milliseconds (instead of string) for calculation.

--- app/spotify/spotify_playlist.py
+++ app/spotify/spotify_playlist.py
@@ -3,3 +3,11 @@ class SpotifyPlaylist:
         self.id = "n/a"
         self.name = "n/a"
         self.tracks = []
+
+    def get_total_duration_ms(self):
+        total_duration_ms = 0
+
+        for track in self.tracks:
+            total_duration_ms += track.duration_ms
+
+        return total_duration_ms
