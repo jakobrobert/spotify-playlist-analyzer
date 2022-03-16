ecc642c86dc7937ce3646d20b35e462d38bf1dae
jakobrobert <jakob-robert@hotmail.de>
2022-03-16 21:42:01 +0100

    Show playlist duration as formatted string

--- app/spotify/spotify_playlist.py
+++ app/spotify/spotify_playlist.py
@@ -4,10 +4,20 @@ class SpotifyPlaylist:
         self.name = "n/a"
         self.tracks = []
 
-    def get_total_duration_ms(self):
-        total_duration_ms = 0
+    def get_duration_ms(self):
+        duration_ms = 0
 
         for track in self.tracks:
-            total_duration_ms += track.duration_ms
+            duration_ms += track.duration_ms
 
-        return total_duration_ms
+        return duration_ms
+
+    def get_duration_string(self):
+        total_milliseconds = self.get_duration_ms()
+        total_seconds = total_milliseconds // 1000
+        total_minutes = total_seconds // 60
+        remaining_seconds = total_seconds % 60
+        total_hours = total_minutes // 60
+        remaining_minutes = total_minutes % 60
+
+        return f"{total_hours:02d}:{remaining_minutes:02d}:{remaining_seconds:02d}"
