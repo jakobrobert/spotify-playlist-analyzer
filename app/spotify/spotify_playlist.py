ca96f8af6b1a901c0a4ac0b4297f80189acc75af
jakobrobert <jakob-robert@hotmail.de>
2022-03-20 20:03:51 +0100

    Show average tempo

--- app/spotify/spotify_playlist.py
+++ app/spotify/spotify_playlist.py
@@ -34,3 +34,11 @@ class SpotifyPlaylist:
         average_duration_ms = int(self.get_average_duration_ms())
 
         return SpotifyTrack.get_duration_string_helper(average_duration_ms)
+
+    def get_average_tempo(self):
+        total_tempo = 0.0
+
+        for track in self.tracks:
+            total_tempo += track.tempo
+
+        return total_tempo / len(self.tracks)
