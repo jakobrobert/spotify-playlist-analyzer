2ffa91f4a2f987b3548de9861c9ab80651bb3281
jakobrobert <jakob-robert@hotmail.de>
2022-05-05 21:14:13 +0200

    #50 Extract conversion to string into methods (#70)
    * Extract methods for SpotifyTrack
    
    * Clean up: Use interpolated strings
    
    * Extract method percentage_to_string()

--- app/spotify/spotify_track.py
+++ app/spotify/spotify_track.py
@@ -14,6 +14,18 @@ class SpotifyTrack:
     def get_duration_string(self):
         return self.get_duration_string_helper(self.duration_ms)
 
+    def get_artists_string(self):
+        return ", ".join(self.artists)
+
+    def get_tempo_string(self):
+        return f"{self.tempo:.1f}"
+
+    def get_loudness_string(self):
+        return f"{self.loudness:.1f}"
+
+    def get_genres_string(self):
+        return ", ".join(self.genres)
+
     @staticmethod
     def get_duration_string_helper(duration_ms):
         total_seconds = duration_ms // 1000
