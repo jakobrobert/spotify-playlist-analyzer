a83e267387d6ef4cafdafe651985e7177af10286
jakobrobert <jakob-robert@hotmail.de>
2022-05-10 21:11:54 +0200

    Cleanup: Re-order attributes and methods in SpotifyTrack for consistency

--- app/spotify/spotify_track.py
+++ app/spotify/spotify_track.py
@@ -17,14 +17,14 @@ class SpotifyTrack:
     def get_duration_string(self):
         return self.get_duration_string_helper(self.duration_ms)
 
+    def get_genres_string(self):
+        return ", ".join(self.genres)
+
     def get_tempo_string(self):
         return f"{self.tempo:.1f}"
 
     def get_loudness_string(self):
         return f"{self.loudness:.1f}"
-
-    def get_genres_string(self):
-        return ", ".join(self.genres)
 
     @staticmethod
     def get_duration_string_helper(duration_ms):
