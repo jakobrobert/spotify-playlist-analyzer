cfc50b0f775727ba081255eb27b28909d9f7c119
jakobrobert <jakob-robert@hotmail.de>
2022-05-10 21:42:27 +0200

    #69 Store key and mode as number (#73)
    * Store key as number
    
    * Cleanup: Re-order attributes and methods in SpotifyTrack for consistency
    
    * Add method to convert key to string
    
    * Clean up
    
    * Fix camelot
    
    * Store mode as number
    
    * Add method to convert mode to string
    
    * Make invalid check for key more safe, this way is consistent to check for mode
    
    * Fix camelot
    
    * Clean up loop in playlist.html

--- app/spotify/spotify_track.py
+++ app/spotify/spotify_track.py
@@ -1,4 +1,6 @@
 class SpotifyTrack:
+    KEY_NAMES = ["C", "C♯/D♭", "D", "D♯/E♭", "E", "F", "F♯/G♭", "G", "G♯/A♭", "A", "A♯/B♭", "B"]
+
     def __init__(self):
         self.title = "n/a"
         self.artists = []
@@ -6,25 +8,40 @@ class SpotifyTrack:
         self.year_of_release = 0
         self.genres = []
         self.tempo = 0
-        self.key = "n/a"
-        self.mode = "n/a"
+        self.key = -1
+        self.mode = -1
         self.camelot = "n/a"
         self.loudness = 0
-
-    def get_duration_string(self):
-        return self.get_duration_string_helper(self.duration_ms)
 
     def get_artists_string(self):
         return ", ".join(self.artists)
 
-    def get_tempo_string(self):
-        return f"{self.tempo:.1f}"
-
-    def get_loudness_string(self):
-        return f"{self.loudness:.1f}"
+    def get_duration_string(self):
+        return self.get_duration_string_helper(self.duration_ms)
 
     def get_genres_string(self):
         return ", ".join(self.genres)
+
+    def get_tempo_string(self):
+        return f"{self.tempo:.1f}"
+
+    def get_key_string(self):
+        if self.key < 0 or self.key >= len(SpotifyTrack.KEY_NAMES):
+            return "n/a"
+
+        return SpotifyTrack.KEY_NAMES[self.key]
+
+    def get_mode_string(self):
+        if self.mode == 0:
+            return "Minor"
+
+        if self.mode == 1:
+            return "Major"
+
+        return "n/a"
+
+    def get_loudness_string(self):
+        return f"{self.loudness:.1f}"
 
     @staticmethod
     def get_duration_string_helper(duration_ms):
