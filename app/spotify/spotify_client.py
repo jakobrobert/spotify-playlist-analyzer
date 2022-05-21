bfaaa51b15913092ca8ed008aebf10f7921d25e4
jakobrobert <jakob-robert@hotmail.de>
2022-05-21 14:52:05 +0200

    Small fix: Pass access_token to __send_get_request_with_ids and not headers.

--- app/spotify/spotify_client.py
+++ app/spotify/spotify_client.py
@@ -127,13 +127,12 @@ class SpotifyClient:
         artist_id_to_genres = {}
 
         url = "https://api.spotify.com/v1/artists"
-        headers = {"Authorization": f"Bearer {access_token}"}
-
         max_ids_per_request = 50
         artist_id_chunks = SpotifyClient.__split_list_into_chunks(artist_ids, max_ids_per_request)
 
         for curr_artist_ids in artist_id_chunks:
-            SpotifyClient.__get_artist_id_to_genres_for_one_request(curr_artist_ids, url, headers, artist_id_to_genres)
+            SpotifyClient.__get_artist_id_to_genres_for_one_request(curr_artist_ids, url, access_token,
+                                                                    artist_id_to_genres)
 
         return artist_id_to_genres
 
@@ -150,8 +149,8 @@ class SpotifyClient:
 
     # TODO CLEANUP out param is dirty, better: return dict for this request, merge data outside of this function
     @staticmethod
-    def __get_artist_id_to_genres_for_one_request(artist_ids, url, headers, artist_id_to_genres):
-        response_data = SpotifyClient.__send_get_request_with_ids(url, headers, artist_ids)
+    def __get_artist_id_to_genres_for_one_request(artist_ids, url, access_token, artist_id_to_genres):
+        response_data = SpotifyClient.__send_get_request_with_ids(url, access_token, artist_ids)
 
         artists = response_data["artists"]
         for artist in artists:
