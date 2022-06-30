5774b1cb3eb591a796dbe13a98235a2af2c539c4
jakobrobert <jakob-robert@hotmail.de>
2022-06-30 22:23:57 +0200

    Adjust port for REST API dev deployment

--- api/run_development.sh
+++ api/run_development.sh
@@ -3,5 +3,5 @@ cd app/ || exit
 export FLASK_APP=spotify_playlist_analyzer_api:app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=1024
+export FLASK_RUN_PORT=1025
 flask run
