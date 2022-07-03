2ebac7452e5cd67d4b5a1adf09daf4f89fe24a5b
jakobrobert <jakob-robert@hotmail.de>
2022-07-03 19:12:47 +0200

    Add sort & filter functionality to API playlist endpoint

--- api/app/spotify_playlist_analyzer_api.py
+++ api/app/spotify_playlist_analyzer_api.py
@@ -1,4 +1,6 @@
-from flask import Flask, jsonify
+import operator
+
+from flask import Flask, jsonify, request
 
 import configparser
 
@@ -25,6 +27,99 @@ def index():
 def get_playlist_by_id(playlist_id):
     playlist = spotify_client.get_playlist_by_id(playlist_id)
 
-    # TODO LATER: apply sorting & filters. for now, keep in client
+    sort_by = request.args.get("sort_by") or "none"
+    order = request.args.get("order") or "ascending"
+    __sort_tracks(playlist.tracks, sort_by, order)
+
+    filter_by = request.args.get("filter_by") or None
+    artists_substring = request.args.get("artists_substring") or None
+    title_substring = request.args.get("title_substring") or None
+    min_release_year = __get_request_param_as_int_or_none("min_release_year")
+    max_release_year = __get_request_param_as_int_or_none("max_release_year")
+    min_tempo = __get_request_param_as_int_or_none("min_tempo")
+    max_tempo = __get_request_param_as_int_or_none("max_tempo")
+    expected_key = request.args.get("expected_key") or None
+    expected_mode = request.args.get("expected_mode") or None
+    genres_substring = request.args.get("genres_substring") or None
+    playlist.tracks = __filter_tracks(
+        playlist.tracks, filter_by, min_tempo, max_tempo, min_release_year, max_release_year,
+        artists_substring, genres_substring, expected_key, expected_mode, title_substring
+    )
+
+    # TODO fix: when using toJSON() instead of jsonify, Browser shows as raw text, probably mime type not correct
+    #   -> idea: convert to dict using __dict__, then use jsonify. then need to convert tracks to dict as well
 
     return playlist.toJSON()
+
+
+def __sort_tracks(tracks, sort_by, order):
+    if sort_by == "none":
+        return
+
+    reverse = (order == "descending")
+    tracks.sort(key=operator.attrgetter(sort_by), reverse=reverse)
+
+
+def __filter_tracks(tracks, filter_by, min_tempo, max_tempo, min_release_year, max_release_year,
+                    artists_substring, genres_substring, expected_key, expected_mode, title_substring):
+    if filter_by is None:
+        return tracks
+
+    if filter_by == "artists":
+        if artists_substring is None:
+            raise ValueError("artists_substring must be defined to filter by artists!")
+
+        return list(filter(lambda track: any(artists_substring in artist for artist in track.artists), tracks))
+
+    if filter_by == "title":
+        if title_substring is None:
+            raise ValueError("title_substring must be defined to filter by title!")
+
+        return list(filter(lambda track: title_substring in track.title, tracks))
+
+    if filter_by == "release_year":
+        if min_release_year is None:
+            raise ValueError("min_release_year must be defined to filter by release_year!")
+
+        if max_release_year is None:
+            raise ValueError("max_release_year must be defined to filter by release_year!")
+
+        return list(filter(lambda track: min_release_year <= track.release_year <= max_release_year, tracks))
+
+    if filter_by == "tempo":
+        if min_tempo is None:
+            raise ValueError("min_tempo must be defined to filter by tempo!")
+
+        if max_tempo is None:
+            raise ValueError("max_tempo must be defined to filter by tempo!")
+
+        return list(filter(lambda track: min_tempo <= track.tempo <= max_tempo, tracks))
+
+    if filter_by == "key":
+        if expected_key is None:
+            raise ValueError("expected_key must be defined to filter by key!")
+
+        return list(filter(lambda track: track.get_key_string() == expected_key, tracks))
+
+    if filter_by == "mode":
+        if expected_mode is None:
+            raise ValueError("expected_mode must be defined to filter by mode!")
+
+        return list(filter(lambda track: track.get_mode_string() == expected_mode, tracks))
+
+    if filter_by == "genres":
+        if genres_substring is None:
+            raise ValueError("genres_substring must be defined to filter by genres!")
+
+        return list(filter(lambda track: any(genres_substring in genre for genre in track.genres), tracks))
+
+    raise ValueError(f"This attribute is not supported to filter by: {filter_by}")
+
+
+def __get_request_param_as_int_or_none(name):
+    value_string = request.args.get(name) or None
+
+    if value_string:
+        return int(value_string)
+
+    return None
