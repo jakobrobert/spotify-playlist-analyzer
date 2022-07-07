a3dc10734bbe70464f41ecd00f14c40edb9d9868
jakobrobert <jakob-robert@hotmail.de>
2022-07-07 21:39:28 +0200

    API: Remove obsolete code from SpotifyTrack & SpotifyPlaylist

--- api/app/spotify/spotify_playlist.py
+++ api/app/spotify/spotify_playlist.py
@@ -14,63 +14,6 @@ class SpotifyPlaylist:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__)
 
-    # TODO in API, can be static. but first clarify: should string conversion be done in API or on client side?
-    # Cannot be static, else template code cannot access it
-    def percentage_to_string(self, percentage):
-        return f"{percentage:.1f}"
-
-    def get_total_duration_ms(self):
-        total_duration_ms = 0
-
-        for track in self.tracks:
-            total_duration_ms += track.duration_ms
-
-        return total_duration_ms
-
-    def get_total_duration_string(self):
-        total_milliseconds = self.get_total_duration_ms()
-        total_seconds = total_milliseconds // 1000
-        total_minutes = total_seconds // 60
-        remaining_seconds = total_seconds % 60
-        total_hours = total_minutes // 60
-        remaining_minutes = total_minutes % 60
-
-        return f"{total_hours:02d}:{remaining_minutes:02d}:{remaining_seconds:02d}"
-
-    def get_average_duration_ms(self):
-        return self.get_total_duration_ms() / len(self.tracks)
-
-    def get_average_duration_string(self):
-        average_duration_ms = int(self.get_average_duration_ms())
-
-        return SpotifyTrack.get_duration_string_helper(average_duration_ms)
-
-    def get_average_release_year(self):
-        total_year = 0.0
-
-        for track in self.tracks:
-            total_year += track.release_year
-
-        return total_year / len(self.tracks)
-
-    def get_average_release_year_string(self):
-        average_year = self.get_average_release_year()
-
-        return f"{average_year:.1f}"
-
-    def get_average_tempo(self):
-        total_tempo = 0.0
-
-        for track in self.tracks:
-            total_tempo += track.tempo
-
-        return total_tempo / len(self.tracks)
-
-    def get_average_tempo_string(self):
-        average_tempo = self.get_average_tempo()
-
-        return f"{average_tempo:.1f}"
-
     def get_release_year_interval_to_percentage(self):
         first_interval_max_year = 1969
         last_interval_min_year = 2020
@@ -154,4 +97,3 @@ class SpotifyPlaylist:
             percentages_dict[key] = proportion * 100.0
 
         return percentages_dict
-
--- api/app/spotify/spotify_track.py
+++ api/app/spotify/spotify_track.py
@@ -15,18 +15,6 @@ class SpotifyTrack:
         self.camelot = "n/a"
         self.loudness = 0
 
-    def get_artists_string(self):
-        return ", ".join(self.artists)
-
-    def get_duration_string(self):
-        return self.get_duration_string_helper(self.duration_ms)
-
-    def get_genres_string(self):
-        return ", ".join(self.genres)
-
-    def get_tempo_string(self):
-        return f"{self.tempo:.1f}"
-
     def get_key_string(self):
         if self.key < 0 or self.key >= len(SpotifyTrack.KEY_NAMES):
             return "n/a"
@@ -41,14 +29,3 @@ class SpotifyTrack:
             return "Major"
 
         return "n/a"
-
-    def get_loudness_string(self):
-        return f"{self.loudness:.1f}"
-
-    @staticmethod
-    def get_duration_string_helper(duration_ms):
-        total_seconds = duration_ms // 1000
-        total_minutes = total_seconds // 60
-        remaining_seconds = total_seconds % 60
-
-        return f"{total_minutes:02d}:{remaining_seconds:02d}"
