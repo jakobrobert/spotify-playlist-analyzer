d8c3f9345e27ddc06efada1585e0630f75f46e47
jakobrobert <jakob-robert@hotmail.de>
2022-07-10 19:18:58 +0200

    App: Inline getters

--- flask-app/app/spotify/spotify_playlist.py
+++ flask-app/app/spotify/spotify_playlist.py
@@ -17,13 +17,8 @@ class SpotifyPlaylist:
     def percentage_to_string(self, percentage):
         return f"{percentage:.1f}"
 
-    # TODO inline getters which directly return variable
-    def get_total_duration_ms(self):
-        return self.total_duration_ms
-
     def get_total_duration_string(self):
-        total_milliseconds = self.get_total_duration_ms()
-        total_seconds = total_milliseconds // 1000
+        total_seconds = self.total_duration_ms // 1000
         total_minutes = total_seconds // 60
         remaining_seconds = total_seconds % 60
         total_hours = total_minutes // 60
@@ -31,26 +26,11 @@ class SpotifyPlaylist:
 
         return f"{total_hours:02d}:{remaining_minutes:02d}:{remaining_seconds:02d}"
 
-    def get_average_duration_ms(self):
-        return self.average_duration_ms
-
     def get_average_duration_string(self):
-        average_duration_ms = int(self.get_average_duration_ms())
-
-        return SpotifyTrack.get_duration_string_helper(average_duration_ms)
-
-    def get_average_release_year(self):
-        return self.average_release_year
+        return SpotifyTrack.get_duration_string_helper(int(self.average_duration_ms))
 
     def get_average_release_year_string(self):
-        average_year = self.get_average_release_year()
-
-        return f"{average_year:.1f}"
-
-    def get_average_tempo(self):
-        return self.average_tempo
+        return f"{self.average_release_year :.1f}"
 
     def get_average_tempo_string(self):
-        average_tempo = self.get_average_tempo()
-
-        return f"{average_tempo:.1f}"
+        return f"{self.average_tempo :.1f}"
