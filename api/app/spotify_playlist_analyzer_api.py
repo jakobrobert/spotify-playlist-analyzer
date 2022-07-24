430a400fe09f5a8afe2ece4108403b5bbcb4a8f1
jakobrobert <jakob-robert@hotmail.de>
2022-07-24 20:27:55 +0200

    API: Do one big try-except clause in get_playlist_by_id to catch any exception, but catch HttpError first because this is more specific

--- api/app/spotify_playlist_analyzer_api.py
+++ api/app/spotify_playlist_analyzer_api.py
@@ -22,62 +22,53 @@ app = Flask(__name__)
 def get_playlist_by_id(playlist_id):
     try:
         playlist = spotify_client.get_playlist_by_id(playlist_id)
-    except HttpError as error:
-        return __create_error_response(error)
 
-    sort_by = request.args.get("sort_by") or "none"
-    order = request.args.get("order") or "ascending"
+        sort_by = request.args.get("sort_by") or "none"
+        order = request.args.get("order") or "ascending"
 
-    try:
         __sort_tracks(playlist.tracks, sort_by, order)
-    except Exception as e:
-        error = HttpError(400, repr(e))
-        return __create_error_response(error)
 
-    filter_by = request.args.get("filter_by") or None
-    artists_substring = request.args.get("artists_substring") or None
-    title_substring = request.args.get("title_substring") or None
-    min_release_year = __get_request_param_as_int_or_none("min_release_year")
-    max_release_year = __get_request_param_as_int_or_none("max_release_year")
-    min_tempo = __get_request_param_as_int_or_none("min_tempo")
-    max_tempo = __get_request_param_as_int_or_none("max_tempo")
-    expected_key = request.args.get("expected_key") or None
-    expected_mode = request.args.get("expected_mode") or None
-    expected_key_signature = request.args.get("expected_key_signature") or None
-    genres_substring = request.args.get("genres_substring") or None
+        filter_by = request.args.get("filter_by") or None
+        artists_substring = request.args.get("artists_substring") or None
+        title_substring = request.args.get("title_substring") or None
+        min_release_year = __get_request_param_as_int_or_none("min_release_year")
+        max_release_year = __get_request_param_as_int_or_none("max_release_year")
+        min_tempo = __get_request_param_as_int_or_none("min_tempo")
+        max_tempo = __get_request_param_as_int_or_none("max_tempo")
+        expected_key = request.args.get("expected_key") or None
+        expected_mode = request.args.get("expected_mode") or None
+        expected_key_signature = request.args.get("expected_key_signature") or None
+        genres_substring = request.args.get("genres_substring") or None
 
-    try:
         playlist.tracks = __filter_tracks(
             playlist.tracks, filter_by,
             artists_substring, title_substring, min_release_year, max_release_year, min_tempo, max_tempo,
             expected_key, expected_mode, expected_key_signature, genres_substring)
-    except Exception as e:
-        error = HttpError(400, repr(e))
-        return __create_error_response(error)
 
-    # Need to explicitly copy the dict, else changing the dict would change the original object
-    playlist_dict = dict(playlist.__dict__)
+        # Need to explicitly copy the dict, else changing the dict would change the original object
+        playlist_dict = dict(playlist.__dict__)
 
-    # Add calculated values
-    try:
+        # Add calculated values
         playlist_dict["total_duration_ms"] = playlist.get_total_duration_ms()
         playlist_dict["average_duration_ms"] = playlist.get_average_duration_ms()
         playlist_dict["average_release_year"] = playlist.get_average_release_year()
         playlist_dict["average_tempo"] = playlist.get_average_tempo()
+
+        # Need to convert tracks to dict manually, playlist.__dict__ does not work recursively
+        playlist_dict["tracks"] = []
+        for track in playlist.tracks:
+            track_dict = dict(track.__dict__)
+            # Overwrite values for key & mode so API returns them as strings instead of numbers
+            track_dict["key"] = track.get_key_string()
+            track_dict["mode"] = track.get_mode_string()
+            playlist_dict["tracks"].append(track_dict)
+
+        return jsonify(playlist_dict)
+    except HttpError as error:
+        return __create_error_response(error)
     except Exception as e:
         error = HttpError(400, repr(e))
         return __create_error_response(error)
-
-    # Need to convert tracks to dict manually, playlist.__dict__ does not work recursively
-    playlist_dict["tracks"] = []
-    for track in playlist.tracks:
-        track_dict = dict(track.__dict__)
-        # Overwrite values for key & mode so API returns them as strings instead of numbers
-        track_dict["key"] = track.get_key_string()
-        track_dict["mode"] = track.get_mode_string()
-        playlist_dict["tracks"].append(track_dict)
-
-    return jsonify(playlist_dict)
 
 
 def __create_error_response(error):
