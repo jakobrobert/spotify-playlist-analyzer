f15d1c7da416a8e250eb9d47a829650d15578119
jakobrobert <jakob-robert@hotmail.de>
2022-07-24 20:44:59 +0200

    #128 Extend extend error handling (#129)
    * API: Change ValueError to HttpError in SpotifyClient
    
    * API: Add missing try-except in get_attribute_distribution_of_playlist
    
    * API: Extract duplicated code into function __create_error_response
    
    * API: Return error response for invalid attribute in get_attribute_distribution_of_playlist
    
    * App: Handle exception raised by __get_attribute_name
    
    * API: Return error response if there occurs an exception in __sort_tracks
    
    * App: Use repr to explicitly convert exception to string. It worked by luck with ValueError, but e.g. not with AttributeError
    
    * App: Add missing try except in compare_attribute_distribution_of_playlists
    
    * API: Handle error raised by __filter_tracks
    
    * API: Return error response if there occurs an exception during calculations in get_playlist_by_id & remove TODO about error code 500, does not matter
    
    * API: Do one big try-except clause in get_playlist_by_id to catch any exception, but catch HttpError first because this is more specific
    
    * API: Do one big try-except clause in get_attribute_distribution_of_playlist
    
    * API: Add general try except to other endpoints as well
    
    * App: Add general try-except for each endpoint
    
    * App: Clean up try-except clauses

--- api/app/spotify/spotify_client.py
+++ api/app/spotify/spotify_client.py
@@ -14,7 +14,7 @@ class SpotifyClient:
 
     def get_playlist_by_id(self, playlist_id):
         if playlist_id is None:
-            raise ValueError("playlist_id is None!")
+            raise HttpError(400, "playlist_id is None!")
 
         url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
         access_token = self.__get_access_token()
