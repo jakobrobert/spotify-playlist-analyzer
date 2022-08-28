682094efd41f652a8ad1674b4534f913e1bb23c1
jakobrobert <jakob-robert@hotmail.de>
2022-08-28 20:13:50 +0200

    Move choose_one_playlist into a Blueprint. Change naming convention: _view suffix for each blueprint to ensure endpoints have different name than the module

--- flask-app/app/views/choose_one_playlist_view.py
+++ flask-app/app/views/choose_one_playlist_view.py
@@ -4,14 +4,14 @@ from http_error import HttpError
 
 from flask import Blueprint, render_template
 
-index = Blueprint("index", __name__)
+choose_one_playlist_view = Blueprint("choose_one_playlist_view", __name__)
 
 
 # TODO CLEANUP use URL_PREFIX constant. or pass url_prefix to app.register_blueprint, but did not work as expected
-@index.route("/spotify-playlist-analyzer/dev/", methods=["GET"])
-def get_index():
+@choose_one_playlist_view.route("/spotify-playlist-analyzer/dev/" + "choose-one-playlist", methods=["GET"])
+def choose_one_playlist():
     try:
-        return render_template("index.html")
+        return render_template("choose_one_playlist.html")
     except Exception as e:
         error = HttpError(502, repr(e))
         return render_template("error.html", error=error)
---
+++ flask-app/app/views/index_view.py
@@ -0,0 +1,18 @@
+# PyCharm shows errors for this import locally, but it works this way with the server
+# 'from app.http_error import HttpError' is shown as valid locally, but does not work with the server
+from http_error import HttpError
+
+from flask import Blueprint, render_template
+
+index_view = Blueprint("index_view", __name__)
+
+
+# TODO CLEANUP use URL_PREFIX constant. or pass url_prefix to app.register_blueprint, but did not work as expected
+@index_view.route("/spotify-playlist-analyzer/dev/", methods=["GET"])
+def index():
+    # TODO re-enable exception handling
+    #try:
+        return render_template("index.html")
+    #except Exception as e:
+        #error = HttpError(502, repr(e))
+        #return render_template("error.html", error=error)
