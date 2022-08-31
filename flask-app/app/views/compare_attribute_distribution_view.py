15572798e3c8a94135673e9ef5e6118490fbddd3
jakobrobert <jakob-robert@hotmail.de>
2022-08-31 22:43:15 +0200

    #134 Refactor Flask app: Split flask code into multiple files (#152)
    * Extract Flask code for index route into separate file, use Blueprint
    
    * Clean up comments & logging
    
    * Move choose_one_playlist into a Blueprint. Change naming convention: _view suffix for each blueprint to ensure endpoints have different name than the module
    
    * Move playlist into Blueprint
    
    * Use URL_PREFIX in view modules. Reading it again from server.ini for each view, probably not super clean, but okay for now.
    
    * Move attribute_distribution into a Blueprint
    
    * Move choose_playlists_for_comparison into a Blueprint
    
    * Move compare_playlists into a Blueprint
    
    * Move compare_attribute_distribution into a Blueprint
    
    * Move choose_one_track into a Blueprint
    
    * Move track_view into a Blueprint
    
    * Remove unused imports
    
    * Remove now obsolete code from spotify_playlist_analyzer.py
    
    * Extract method get_playlist_id_from_playlist_url into new class ViewUtils
    
    * Extract method get_attribute_display_name into new class ViewUtils
    
    * Extract method get_image_base64_from_plot into class ViewUtils

---
+++ flask-app/app/views/compare_attribute_distribution_view.py
@@ -0,0 +1,79 @@
+# PyCharm shows errors for these imports locally, but it works this way with the server
+# 'from app.http_error import HttpError' is shown as valid locally, but does not work with the server
+from api_client import ApiClient
+from http_error import HttpError
+from views.view_utils import ViewUtils
+
+from flask import Blueprint, render_template, request
+import configparser
+import matplotlib.pyplot as plt
+
+config = configparser.ConfigParser()
+config.read("../server.ini")
+URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
+API_BASE_URL = config["DEFAULT"]["API_BASE_URL"]
+
+api_client = ApiClient(API_BASE_URL)
+
+compare_attribute_distribution_view = Blueprint("compare_attribute_distribution_view", __name__)
+
+
+@compare_attribute_distribution_view.route(URL_PREFIX + "compare-attribute-distribution-of-playlists", methods=["GET"])
+def compare_attribute_distribution_of_playlists():
+    try:
+        playlist_id_1 = request.args.get("playlist_id_1")
+        playlist_id_2 = request.args.get("playlist_id_2")
+        attribute = request.args.get("attribute")
+
+        attribute_name = ViewUtils.get_attribute_display_name(attribute, api_client)
+
+        # TODO optimize: separate request to get playlist is overkill, see attribute_distribution_view.py
+        playlist_1 = api_client.get_playlist_by_id(playlist_id_1)
+        playlist_2 = api_client.get_playlist_by_id(playlist_id_2)
+        attribute_value_to_percentage_1 = api_client.get_attribute_distribution_of_playlist(playlist_id_1, attribute)
+        attribute_value_to_percentage_2 = api_client.get_attribute_distribution_of_playlist(playlist_id_2, attribute)
+
+        return __render_compare_attribute_distribution_template(
+            playlist_1, playlist_2, attribute_name, attribute_value_to_percentage_1, attribute_value_to_percentage_2)
+    except HttpError as error:
+        return render_template("error.html", error=error)
+    except Exception as e:
+        error = HttpError(502, repr(e))
+        return render_template("error.html", error=error)
+
+
+def __render_compare_attribute_distribution_template(
+        playlist_1, playlist_2, attribute_name, attribute_value_to_percentage_1, attribute_value_to_percentage_2):
+    chart_image_base64 = __get_attribute_comparison_chart_image_base64(
+        attribute_name, playlist_1.name, playlist_2.name,
+        attribute_value_to_percentage_1, attribute_value_to_percentage_2
+    )
+
+    return render_template("compare_attribute_distribution.html",
+                           playlist_1=playlist_1, playlist_2=playlist_2, attribute_name=attribute_name,
+                           attribute_value_to_percentage_1=attribute_value_to_percentage_1,
+                           attribute_value_to_percentage_2=attribute_value_to_percentage_2,
+                           chart_image_base64=chart_image_base64)
+
+
+def __get_attribute_comparison_chart_image_base64(attribute_name, playlist_name_1, playlist_name_2,
+                                                  attribute_value_to_percentage_1, attribute_value_to_percentage_2):
+    plt.title(f"Compare {attribute_name} Distribution")
+    plt.xlabel(attribute_name)
+    plt.ylabel("Percentage")
+
+    x_labels = []
+    y_labels_1 = []
+    y_labels_2 = []
+    for item_1, item_2 in zip(attribute_value_to_percentage_1, attribute_value_to_percentage_2):
+        x_labels.append(item_1["label"])
+        y_labels_1.append(item_1["percentage"])
+        y_labels_2.append(item_2["percentage"])
+
+    plt.bar(x_labels, y_labels_1, fill=False, linewidth=2, edgecolor="red", label=playlist_name_1)
+    plt.bar(x_labels, y_labels_2, fill=False, linewidth=2, edgecolor="blue", label=playlist_name_2)
+    plt.xticks(rotation=15)
+    plt.legend()
+    plt.tight_layout()
+
+    return ViewUtils.get_image_base64_from_plot()
