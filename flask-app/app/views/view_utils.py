ce7fdd1919bb9a181a9fb2d1db1c27839304493c
jakobrobert <jakob-robert@hotmail.de>
2022-08-31 22:37:11 +0200

    Extract method get_image_base64_from_plot into class ViewUtils

--- flask-app/app/views/view_utils.py
+++ flask-app/app/views/view_utils.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
+
+
 class ViewUtils:
     @staticmethod
     def get_playlist_id_from_playlist_url(playlist_url):
@@ -15,3 +20,14 @@ class ViewUtils:
                 return attribute["display_name"]
 
         raise ValueError(f"Invalid attribute: '{attribute_name}'")
+
+    @staticmethod
+    def get_image_base64_from_plot():
+        image_buffer = BytesIO()
+        plt.savefig(image_buffer, format="png")
+        plt.clf()  # Clear the current figure. Else the different figures would be drawn on top of each other.
+        image_bytes = image_buffer.getvalue()
+        image_base64_bytes = base64.encodebytes(image_bytes)
+        image_base64_string = image_base64_bytes.decode("utf8")
+
+        return image_base64_string
