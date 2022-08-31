30811a7be4211e17bc6fbded4f626583047e4b4d
jakobrobert <jakob-robert@hotmail.de>
2022-08-31 22:11:24 +0200

    Move choose_one_track into a Blueprint

---
+++ flask-app/app/views/choose_one_track_view.py
@@ -0,0 +1,21 @@
+# PyCharm shows errors for this import locally, but it works this way with the server
+# 'from app.http_error import HttpError' is shown as valid locally, but does not work with the server
+from http_error import HttpError
+
+from flask import Blueprint, render_template
+import configparser
+
+config = configparser.ConfigParser()
+config.read("../server.ini")
+URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
+
+choose_one_track_view = Blueprint("choose_one_track_view", __name__)
+
+
+@choose_one_track_view.route(URL_PREFIX + "choose-one-track", methods=["GET"])
+def choose_one_track():
+    try:
+        return render_template("choose_one_track.html")
+    except Exception as e:
+        error = HttpError(502, repr(e))
+        return render_template("error.html", error=error)
