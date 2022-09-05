4574df064fb11ceb995a524a2aa3197b28880068
jakobrobert <jakob-robert@hotmail.de>
2022-09-05 21:29:54 +0200

    Extract method HttpError.from_last_exception

--- flask-app/app/views/playlist_view.py
+++ flask-app/app/views/playlist_view.py
@@ -85,15 +85,6 @@ def get_playlist_by_id(playlist_id):
     except HttpError as error:
         return render_template("error.html", error=error)
     except Exception as e:
-        # TODO CLEANUP extract helper method, re-use for all generic exceptions
+        error = HttpError.from_last_exception()
         # TODO improve template: print error code, name & traceback separately. traceback should be smaller & not be in website title
-        # Somehow, if then printing ex_traceback, just prints something like <traceback object at 0x7f43916de7c8>,
-        # So using traceback.format_exc() for this.
-        # ex_value just contains less info traceback, so can ignore this
-        ex_type, ex_value, ex_traceback = sys.exc_info()
-        ex_name = ex_type.__name__
-        traceback_text = traceback.format_exc()
-        message = f"{ex_name}: {traceback_text}"
-        error = HttpError(502, message)
-
         return render_template("error.html", error=error)
