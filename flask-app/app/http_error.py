b05725078c3303b9a55230bc3d1ce6823ad1118c
jakobrobert <jakob-robert@hotmail.de>
2022-09-06 20:54:41 +0200

    Use traceback.extract_tb so template code can deal with formatting

--- flask-app/app/http_error.py
+++ flask-app/app/http_error.py
@@ -4,25 +4,24 @@ import traceback
 
 class HttpError(Exception):
     # TODO CLEANUP after all occurrences adjusted, remove message from constructor
-    def __init__(self, status_code, message, name="", traceback_lines=[]):
+    def __init__(self, status_code, message, name="", traceback_items=None):
+        if traceback_items is None:
+            traceback_items = []
+
         self.status_code = status_code
         self.message = message
         self.name = name
-        self.traceback_lines = traceback_lines
+        self.traceback_items = traceback_items
 
     @staticmethod
     def from_last_exception():
-        # Somehow, if then printing ex_traceback, just prints something like <traceback object at 0x7f43916de7c8>,
-        # So using traceback.format_exc() for this.
-        # ex_value just contains less info traceback, so can ignore this
+        # ex_value contains less info than traceback and no additional info, so can ignore this
         ex_type, ex_value, ex_traceback = sys.exc_info()
         ex_name = ex_type.__name__
-        traceback_text = traceback.format_exc()
-        # TODO improve formatting, proper indentation -> can retrieve info from traceback and format manually using extract_tb, see here:
-        #  https://stackoverflow.com/questions/4690600/python-exception-message-capturing
-        # then can handle proper indentation in the HTML template
-        # should NOT replace newlines and spaces by HTML tags in python code, this would be ugly
-        traceback_lines = traceback_text.splitlines()
 
-        return HttpError(502, "", ex_name, traceback_lines)
+        # Using this instead so the template code can deal with formatting
+        # With traceback.format_exc()
+        traceback_items = traceback.extract_tb(ex_traceback)
+
+        return HttpError(502, "", ex_name, traceback_items)
 
