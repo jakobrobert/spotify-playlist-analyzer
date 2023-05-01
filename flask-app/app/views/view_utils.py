5fa63636a74aaa335387e3f800a59f295929eb96
jakobrobert <jakob-robert@hotmail.de>
2023-05-01 12:46:55 +0200

    #150 Fix: Make parsing of playlist and track url more robust (#165)
    * For playlist url, if url_prefix 'playlist/' is missing, raise ValueError to show meaningful error message
    
    * Make parsing playlist url more robust, make it also work if "?si=" part is omitted
    
    * Move get_track_id_from_track_url from track_view.py into view_utils.py. This way, is consistent to get_playlist_id_from_playlist_url, even if only used once for track id
    
    * Make get_track_id_from_track_url more robust, basically copy pasted from get_playlist_id_from_playlist_url

--- flask-app/app/views/view_utils.py
+++ flask-app/app/views/view_utils.py
@@ -6,10 +6,37 @@ import base64
 class ViewUtils:
     @staticmethod
     def get_playlist_id_from_playlist_url(playlist_url):
-        start_index = playlist_url.find("playlist/") + len("playlist/")
-        end_index = playlist_url.find("?")
+        url_prefix = "playlist/"
+        url_prefix_start_index = playlist_url.find(url_prefix)
 
-        return playlist_url[start_index:end_index]
+        if url_prefix_start_index == -1:
+            raise ValueError(f"Invalid URL for Spotify Playlist, missing \"{url_prefix}\": {playlist_url}")
+
+        id_start_index = playlist_url.find(url_prefix) + len(url_prefix)
+        id_end_index = None
+
+        si_start_index = playlist_url.find("?si=")
+        if si_start_index != -1:
+            id_end_index = si_start_index
+
+        return playlist_url[id_start_index:id_end_index]
+
+    @staticmethod
+    def get_track_id_from_track_url(track_url):
+        url_prefix = "track/"
+        url_prefix_start_index = track_url.find(url_prefix)
+
+        if url_prefix_start_index == -1:
+            raise ValueError(f"Invalid URL for Spotify Track, missing \"{url_prefix}\": {track_url}")
+
+        id_start_index = track_url.find(url_prefix) + len(url_prefix)
+        id_end_index = None
+
+        si_start_index = track_url.find("?si=")
+        if si_start_index != -1:
+            id_end_index = si_start_index
+
+        return track_url[id_start_index:id_end_index]
 
     @staticmethod
     def get_attribute_display_name(attribute_name, api_client):
