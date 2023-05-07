bb9887a2b9f43e872e252e984720aa5ab831e134
jakobrobert <jakob-robert@hotmail.de>
2023-05-07 17:51:54 +0200

    App: Build exported_playlist_url based on id, which will be returned by the API, then use this url in the template

--- flask-app/app/views/export_playlist_view.py
+++ flask-app/app/views/export_playlist_view.py
@@ -16,9 +16,9 @@ export_playlist_view = Blueprint("export_playlist_view", __name__)
 def export_playlist():
     try:
         # TODO use API endpoint to export the playlist, it returns id of the new playlist
-        # TODO build playlist url based on id
-        # TODO pass playlist url to template
-        return render_template("export_playlist.html")
+        exported_playlist_id = "0Q4lgHJpZo7DpZRygCGlGs"
+        exported_playlist_url = f"https://open.spotify.com/playlist/{exported_playlist_id}"
+        return render_template("export_playlist.html", exported_playlist_url=exported_playlist_url)
     except Exception:
         error = HttpError.from_last_exception()
         return render_template("error.html", error=error)
