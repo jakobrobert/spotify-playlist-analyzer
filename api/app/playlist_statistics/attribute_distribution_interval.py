bccc9b2dce01ce94f85abe055e691b5a62eba4b3
jakobrobert <jakob-robert@hotmail.de>
2023-05-16 09:00:46 +0200

    API: Move update_percentage calls from get_duration_interval_to_percentage into __get_attribute_distribution_intervals because it is a general thing, not related to specific attributes

--- api/app/playlist_statistics/attribute_distribution_interval.py
+++ api/app/playlist_statistics/attribute_distribution_interval.py
@@ -5,8 +5,8 @@ class AttributeDistributionInterval:
         self.count = 0
         self.percentage = 0
 
-    def update_percentage(self, total):
-        self.percentage = 100 * self.count / total
+    def update_percentage(self, total_count):
+        self.percentage = 100 * self.count / total_count
 
     # TODO Add method to check if value is in interval
     # TODO maybe add more high level method update_count(all_values)
