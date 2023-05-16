24ed5790eb052e1f68109f8080bb02db17daed11
jakobrobert <jakob-robert@hotmail.de>
2023-05-16 09:34:25 +0200

    API: Fix is_value_in_interval, was just typo, incorrect for case self.max_value is None, i.e. last interval

--- api/app/playlist_statistics/attribute_distribution_interval.py
+++ api/app/playlist_statistics/attribute_distribution_interval.py
@@ -8,13 +8,12 @@ class AttributeDistributionInterval:
     def update_percentage(self, total_count):
         self.percentage = 100 * self.count / total_count
 
-    # TODO there is a bug, values are now different than with version before
     def is_value_in_interval(self, value):
         if self.min_value is None and self.max_value is None:
             return True
         if self.min_value is None:
             return value <= self.max_value
         if self.max_value is None:
-            return value <= self.min_value
+            return value >= self.min_value
 
         return self.min_value <= value <= self.max_value
