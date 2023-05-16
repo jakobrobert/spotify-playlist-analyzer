751eaa3a8f1a52b97905c8a4ca74d97c6367edb3
jakobrobert <jakob-robert@hotmail.de>
2023-05-16 08:55:13 +0200

    API: Extract method update_percentage into AttributeDistributionInterval. Refactor code to first get the attribute distribution intervals, then update percentages, then convert to dict with labels as expected on caller side. Works for duration, rest still needs to be adjusted

--- api/app/playlist_statistics/attribute_distribution_interval.py
+++ api/app/playlist_statistics/attribute_distribution_interval.py
@@ -1,6 +1,13 @@
 class AttributeDistributionInterval:
-    # TODO for now, is just a data container. Move logic into this class. e.g. update_percentage(total_count)
     def __init__(self, min_value, max_value):
         self.min_value = min_value
         self.max_value = max_value
         self.count = 0
+        self.percentage = 0
+
+    def update_percentage(self, total):
+        self.percentage = 100 * self.count / total
+
+    # TODO Add method to check if value is in interval
+    # TODO maybe add more high level method update_count(all_values)
+        # -> e.g. it contains release_year values of all tracks, then counts how many are within this interval
