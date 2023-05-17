766ea0af86603f5721cc4eb9807356ec716d4768
jakobrobert <jakob-robert@hotmail.de>
2023-05-17 09:45:06 +0200

    #178 Refactor: From SpotifyPlaylist, extract class PlaylistStatistics for attribute distribution & average calculation (#180)
    * API: Copy code from SpotifyPlaylist, related to calculation of attribute distribution, into new class PlaylistStatistics
    
    * API: In SpotifyPlaylist, change get_duration_interval_to_percentage to just delegate to PlaylistStatistics, works fine
    
    * API: Change remaining methods so they delegate to PlaylistStatistics
    
    * API: Extract class AttributeDistributionInterval, which contains min_value, max_value, count. Use it in __get_intervals_with_count & transform to dict with "label" & "count" afterwards
    
    * API: Extract method __get_label_for_interval
    
    * API: Fix labels for first & max interval (broke it temporarily before) -> Add special handling in __get_label_for_interval if min_value or max_value is None.
    
    * API: Extract method update_percentage into AttributeDistributionInterval. Refactor code to first get the attribute distribution intervals, then update percentages, then convert to dict with labels as expected on caller side. Works for duration, rest still needs to be adjusted
    
    * API: Move update_percentage calls from get_duration_interval_to_percentage into __get_attribute_distribution_intervals because it is a general thing, not related to specific attributes
    
    * API: Adjust code for remaining attributes which use intervals (for key & mode, it is special logic)
    
    * API: Add general method is_value_in_interval to AttributeDistributionInterval which can be used for first, middle & last interval. TODO: Percentages are now different than with version before, so this change introduces a bug
    
    * API: Fix is_value_in_interval, was just typo, incorrect for case self.max_value is None, i.e. last interval
    
    * API: Use list comprehension to simplify code
    
    * API: Split phases. First, create all intervals, then calculate counts for all intervals
    
    * API: Extract method update_count into AttributeDistributionInterval & make __is_value_in_interval private
    
    * API: Extract helper methods for creating intervals
    
    * API: In __get_attribute_distribution_intervals, get rid of now obsolete  comments. Also move all_values close to first usage.
    
    * API: Move calculation of total duration & all average values into PlaylistStatistics. In SpotifyPlaylist, all methods delegate to statistics object for intermediate step to not break caller code
    
    * API: Remove now unused method get_duration_string from SpotifyPlaylist, has been moved to PlaylistStatistics
    
    * API: Make __convert_counts_to_percentages static by passing total_count.
    
    * API: Directly use PlaylistStatistics object in API endpoints
    
    * API: Swap lines just for preference
    
    * API: In SpotifyPlaylist, remove all the now obsolete code. Now, SpotifyPlaylist is just a stupid data class. But that's fine.

---
+++ api/app/playlist_statistics/attribute_distribution_interval.py
@@ -0,0 +1,22 @@
+class AttributeDistributionInterval:
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+        self.count = 0
+        self.percentage = 0
+
+    def update_count(self, all_values):
+        self.count = sum(self.__is_value_in_interval(value) for value in all_values)
+
+    def update_percentage(self, total_count):
+        self.percentage = 100 * self.count / total_count
+
+    def __is_value_in_interval(self, value):
+        if self.min_value is None and self.max_value is None:
+            return True
+        if self.min_value is None:
+            return value <= self.max_value
+        if self.max_value is None:
+            return value >= self.min_value
+
+        return self.min_value <= value <= self.max_value
---
+++ api/app/playlist_statistics/playlist_statistics.py
@@ -0,0 +1,249 @@
+from spotify.spotify_track import SpotifyTrack
+from playlist_statistics.attribute_distribution_interval import AttributeDistributionInterval
+
+
+class PlaylistStatistics:
+    def __init__(self, tracks):
+        self.tracks = tracks
+
+    def get_total_duration_ms(self):
+        total_duration_ms = 0
+
+        for track in self.tracks:
+            total_duration_ms += track.duration_ms
+
+        return total_duration_ms
+
+    def get_average_duration_ms(self):
+        return self.get_total_duration_ms() / len(self.tracks)
+
+    def get_average_popularity(self):
+        total_popularity = 0.0
+
+        for track in self.tracks:
+            total_popularity += track.popularity
+
+        return total_popularity / len(self.tracks)
+
+    def get_average_release_year(self):
+        total_year = 0.0
+
+        for track in self.tracks:
+            total_year += track.release_year
+
+        return total_year / len(self.tracks)
+
+    def get_average_tempo(self):
+        total_tempo = 0.0
+
+        for track in self.tracks:
+            total_tempo += track.tempo
+
+        return total_tempo / len(self.tracks)
+
+    def get_duration_interval_to_percentage(self):
+        first_interval_max_duration = 120000  # 120 seconds -> 02:00
+        last_interval_min_duration = 300000  # 300 seconds -> 05:00
+        interval_size = 30000  # 30 seconds
+
+        intervals = self.__get_attribute_distribution_intervals(
+            first_interval_max_duration, last_interval_min_duration, interval_size,
+            lambda track: track.duration_ms)
+
+        dicts_with_label = self.__convert_attribute_distribution_intervals_to_dicts_with_label(intervals)
+
+        return dicts_with_label
+
+    def get_release_year_interval_to_percentage(self):
+        first_interval_max_year = 1979
+        last_interval_min_year = 2020
+        interval_size = 10
+
+        intervals = self.__get_attribute_distribution_intervals(
+            first_interval_max_year, last_interval_min_year, interval_size,
+            lambda track: track.release_year)
+
+        dicts_with_label = self.__convert_attribute_distribution_intervals_to_dicts_with_label(intervals)
+
+        return dicts_with_label
+
+    def get_popularity_interval_to_percentage(self):
+        first_interval_max_popularity = 9
+        last_interval_min_popularity = 90
+        interval_size = 10
+
+        intervals = self.__get_attribute_distribution_intervals(
+            first_interval_max_popularity, last_interval_min_popularity, interval_size,
+            lambda track: track.popularity)
+
+        dicts_with_label = self.__convert_attribute_distribution_intervals_to_dicts_with_label(intervals)
+
+        return dicts_with_label
+
+    def get_tempo_interval_to_percentage(self):
+        first_interval_max_tempo = 89
+        last_interval_min_tempo = 180
+        interval_size = 10
+
+        intervals = self.__get_attribute_distribution_intervals(
+            first_interval_max_tempo, last_interval_min_tempo, interval_size,
+            lambda track: track.tempo)
+
+        dicts_with_label = self.__convert_attribute_distribution_intervals_to_dicts_with_label(intervals)
+
+        return dicts_with_label
+
+    def get_key_to_percentage(self):
+        keys_with_count = []
+
+        # Add one item for each key
+        for key_name in SpotifyTrack.KEY_STRINGS:
+            key_with_count = {
+                "label": key_name,
+                "count": 0
+            }
+
+            keys_with_count.append(key_with_count)
+
+        # Calculate count for each key
+        for track in self.tracks:
+            key_with_count = keys_with_count[track.key]
+            key_with_count["count"] += 1
+
+        total_count = len(self.tracks)
+        return PlaylistStatistics.__convert_counts_to_percentages(keys_with_count, total_count)
+
+    def get_mode_to_percentage(self):
+        modes_with_count = []
+
+        # Add one item for each mode
+        for mode_name in SpotifyTrack.MODE_STRINGS:
+            mode_with_count = {
+                "label": mode_name,
+                "count": 0
+            }
+
+            modes_with_count.append(mode_with_count)
+
+        # Calculate count for each mode
+        for track in self.tracks:
+            mode_with_count = modes_with_count[track.mode]
+            mode_with_count["count"] += 1
+
+        total_count = len(self.tracks)
+        return PlaylistStatistics.__convert_counts_to_percentages(modes_with_count, total_count)
+
+    def get_key_signature_to_percentage(self):
+        key_signatures_with_count = []
+
+        # Add one item for each key_signature
+        for key_signature_name in SpotifyTrack.KEY_SIGNATURE_STRINGS:
+            key_signature_with_count = {
+                "label": key_signature_name,
+                "count": 0
+            }
+
+            key_signatures_with_count.append(key_signature_with_count)
+
+        # Calculate count for each key_signature
+        for track in self.tracks:
+            key_signature = track.key_signature
+            key_signature_index = SpotifyTrack.KEY_SIGNATURE_STRINGS.index(key_signature)
+            key_signature_with_count = key_signatures_with_count[key_signature_index]
+            key_signature_with_count["count"] += 1
+
+        total_count = len(self.tracks)
+        return PlaylistStatistics.__convert_counts_to_percentages(key_signatures_with_count, total_count)
+
+    def __get_attribute_distribution_intervals(
+            self, first_interval_max, last_interval_min, interval_size, get_attribute_value_of_track):
+
+        all_intervals = []
+
+        all_intervals.append(PlaylistStatistics.__create_first_interval(first_interval_max))
+        all_intervals.extend(
+            PlaylistStatistics.__create_middle_intervals(first_interval_max, last_interval_min, interval_size))
+        all_intervals.append(PlaylistStatistics.__create_last_interval(last_interval_min))
+
+        all_values = [get_attribute_value_of_track(track) for track in self.tracks]
+
+        for interval in all_intervals:
+            interval.update_count(all_values)
+
+        total_count = len(self.tracks)
+        for interval in all_intervals:
+            interval.update_percentage(total_count)
+
+        return all_intervals
+
+    @staticmethod
+    def __create_first_interval(first_interval_max):
+        return AttributeDistributionInterval(None, first_interval_max)
+
+    @staticmethod
+    def __create_middle_intervals(first_interval_max, last_interval_min, interval_size):
+        middle_intervals = []
+
+        for min_value in range(first_interval_max + 1, last_interval_min, interval_size):
+            max_value = min_value + interval_size - 1
+            middle_intervals.append(AttributeDistributionInterval(min_value, max_value))
+
+        return middle_intervals
+
+    @staticmethod
+    def __create_last_interval(last_interval_min):
+        return AttributeDistributionInterval(last_interval_min, None)
+
+    @staticmethod
+    def __convert_attribute_distribution_intervals_to_dicts_with_label(intervals, get_label_for_value=None):
+        dicts_with_label = []
+
+        # By default, for labels just convert the value to string
+        if get_label_for_value is None:
+            get_label_for_value = str
+
+        for interval in intervals:
+            dict_with_label = {
+                "label": PlaylistStatistics.__get_label_for_interval(
+                    interval.min_value, interval.max_value, get_label_for_value),
+                "count": interval.count,
+                "percentage": interval.percentage
+            }
+            dicts_with_label.append(dict_with_label)
+
+        return dicts_with_label
+
+    @staticmethod
+    def __get_label_for_interval(min_value, max_value, get_label_for_value):
+        if min_value is None:
+            return f"≤ {get_label_for_value(max_value)}"
+
+        if max_value is None:
+            return f"≥ {get_label_for_value(min_value)}"
+
+        return f"{get_label_for_value(min_value)} - {get_label_for_value(max_value)}"
+
+    @staticmethod
+    def __get_duration_string(duration_ms):
+        total_seconds = duration_ms // 1000
+        total_minutes = total_seconds // 60
+        remaining_seconds = total_seconds % 60
+
+        return f"{total_minutes:02d}:{remaining_seconds:02d}"
+
+    # WARNING Still need to keep this method for key, mode, etc.
+    #   -> There cannot use AttributeDistributionInterval, but those are actually not intervals, but categorical values
+    #   -> If we want to refactor that, need to do it differently
+    @staticmethod
+    def __convert_counts_to_percentages(label_count_pairs, total_count):
+        label_percentage_pairs = []
+
+        for interval_with_count in label_count_pairs:
+            percentage = 100 * interval_with_count["count"] / total_count
+            interval_with_percentage = {
+                "label": interval_with_count["label"],
+                "percentage": percentage
+            }
+            label_percentage_pairs.append(interval_with_percentage)
+
+        return label_percentage_pairs
