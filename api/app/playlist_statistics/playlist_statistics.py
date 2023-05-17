741f7624eda2a33aa430d1aedd960ee6f96c933a
jakobrobert <jakob-robert@hotmail.de>
2023-05-17 09:19:57 +0200

    API: Move calculation of total duration & all average values into PlaylistStatistics. In SpotifyPlaylist, all methods delegate to statistics object for intermediate step to not break caller code

--- api/app/playlist_statistics/playlist_statistics.py
+++ api/app/playlist_statistics/playlist_statistics.py
@@ -6,6 +6,41 @@ class PlaylistStatistics:
     def __init__(self, tracks):
         self.tracks = tracks
 
+    def get_total_duration_ms(self):
+        total_duration_ms = 0
+
+        for track in self.tracks:
+            total_duration_ms += track.duration_ms
+
+        return total_duration_ms
+
+    def get_average_duration_ms(self):
+        return self.get_total_duration_ms() / len(self.tracks)
+
+    def get_average_popularity(self):
+        total_popularity = 0.0
+
+        for track in self.tracks:
+            total_popularity += track.popularity
+
+        return total_popularity / len(self.tracks)
+
+    def get_average_release_year(self):
+        total_year = 0.0
+
+        for track in self.tracks:
+            total_year += track.release_year
+
+        return total_year / len(self.tracks)
+
+    def get_average_tempo(self):
+        total_tempo = 0.0
+
+        for track in self.tracks:
+            total_tempo += track.tempo
+
+        return total_tempo / len(self.tracks)
+
     def get_duration_interval_to_percentage(self):
         first_interval_max_duration = 120000  # 120 seconds -> 02:00
         last_interval_min_duration = 300000  # 300 seconds -> 05:00
