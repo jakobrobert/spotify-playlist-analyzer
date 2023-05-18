25f8ab175d70203a2136ba276f151f3cebf69da1
jakobrobert <jakob-robert@hotmail.de>
2023-05-18 13:38:09 +0200

    App: Send playlist_name to API endpoint

--- flask-app/app/views/export_playlist_view.py
+++ flask-app/app/views/export_playlist_view.py
@@ -19,11 +19,16 @@ export_playlist_view = Blueprint("export_playlist_view", __name__)
 @export_playlist_view.route(URL_PREFIX + "export-playlist", methods=["POST"])
 def export_playlist():
     try:
+        # TODO read from form, once implemented in template
+        playlist_name = "BLA BLA"
+        if not playlist_name:
+            raise HttpError(status_code=400, title="export_playlist failed", message="'playlist_name' is None or empty")
+
         track_ids = request.form.getlist("track_ids[]")
         if not track_ids:
             raise HttpError(status_code=400, title="export_playlist failed", message="'track_ids' is None or empty")
 
-        playlist_id = api_client.create_playlist(track_ids)
+        playlist_id = api_client.create_playlist(playlist_name, track_ids)
         playlist_url = f"https://open.spotify.com/playlist/{playlist_id}"
         return render_template("export_playlist.html", exported_playlist_url=playlist_url)
     except HttpError as error:
