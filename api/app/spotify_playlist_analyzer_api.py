a38b160b933693bfa293fc9dd7c93563c938d7b8
jakobrobert <jakob-robert@hotmail.de>
2023-06-06 23:48:56 +0200

    App: Simplify __extract_filter_params_from_request_params by moving assignment of "filter_by" into the helper methods for each attribute. Now, no update() call needed, just return method result directly

--- api/app/spotify_playlist_analyzer_api.py
+++ api/app/spotify_playlist_analyzer_api.py
@@ -294,51 +294,42 @@ def __sort_tracks(tracks, sort_by, order):
 
 def __extract_filter_params_from_request_params(request_params):
     filter_by = request_params.get("filter_by") or None
-    # TODONOW can move this into extracted methods so no need for update, can directly return result of method calls
-    filter_params = {"filter_by": filter_by}
 
     if filter_by is None:
+        filter_params = {"filter_by": None}
         return filter_params
 
     if filter_by == "artists":
-        filter_params.update(__extract_filter_params_from_request_params_for_artists(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_artists(request_params)
 
     if filter_by == "title":
-        filter_params.update(__extract_filter_params_from_request_params_for_title(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_title(request_params)
 
     if filter_by == "genres":
-        filter_params.update(__extract_filter_params_from_request_params_for_genres(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_genres(request_params)
 
     if filter_by == "key":
-        filter_params.update(__extract_filter_params_from_request_params_for_key(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_key(request_params)
 
     if filter_by == "mode":
-        filter_params.update(__extract_filter_params_from_request_params_for_mode(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_mode(request_params)
 
     if filter_by == "key_signature":
-        filter_params.update(__extract_filter_params_from_request_params_for_key_signature(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_key_signature(request_params)
 
     if filter_by == "release_year":
-        filter_params.update(__extract_filter_params_from_request_params_for_release_year(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_release_year(request_params)
 
     if filter_by == "tempo":
-        filter_params.update(__extract_filter_params_from_request_params_for_tempo(request_params))
-        return filter_params
+        return __extract_filter_params_from_request_params_for_tempo(request_params)
 
     raise HttpError(400, "API Error", f"Invalid value for 'filter_by': '{filter_by}'")
 
 
 def __extract_filter_params_from_request_params_for_artists(request_params):
-    filter_params = {}
-    artists_substring = request_params.get("artists_substring")
+    filter_params = {"filter_by": "artists"}
 
+    artists_substring = request_params.get("artists_substring")
     if not artists_substring:
         raise __create_http_error_for_filter_params("artists", "artists_substring")
 
@@ -348,9 +339,9 @@ def __extract_filter_params_from_request_params_for_artists(request_params):
 
 
 def __extract_filter_params_from_request_params_for_title(request_params):
-    filter_params = {}
-    title_substring = request_params.get("title_substring")
+    filter_params = {"filter_by": "title"}
 
+    title_substring = request_params.get("title_substring")
     if not title_substring:
         raise __create_http_error_for_filter_params("title", "title_substring")
 
@@ -360,9 +351,9 @@ def __extract_filter_params_from_request_params_for_title(request_params):
 
 
 def __extract_filter_params_from_request_params_for_genres(request_params):
-    filter_params = {}
-    genres_substring = request_params.get("genres_substring")
+    filter_params = {"filter_by": "genres"}
 
+    genres_substring = request_params.get("genres_substring")
     if not genres_substring:
         raise __create_http_error_for_filter_params("genres", "genres_substring")
 
@@ -371,9 +362,9 @@ def __extract_filter_params_from_request_params_for_genres(request_params):
 
 
 def __extract_filter_params_from_request_params_for_key(request_params):
-    filter_params = {}
-    expected_key = request_params.get("expected_key")
+    filter_params = {"filter_by": "key"}
 
+    expected_key = request_params.get("expected_key")
     if not expected_key:
         raise __create_http_error_for_filter_params("key", "expected_key")
 
@@ -382,9 +373,9 @@ def __extract_filter_params_from_request_params_for_key(request_params):
 
 
 def __extract_filter_params_from_request_params_for_mode(request_params):
-    filter_params = {}
-    expected_mode = request_params.get("expected_mode")
+    filter_params = {"filter_by": "mode"}
 
+    expected_mode = request_params.get("expected_mode")
     if not expected_mode:
         raise __create_http_error_for_filter_params("mode", "expected_mode")
 
@@ -393,9 +384,9 @@ def __extract_filter_params_from_request_params_for_mode(request_params):
 
 
 def __extract_filter_params_from_request_params_for_key_signature(request_params):
-    filter_params = {}
-    expected_key_signature = request_params.get("expected_key_signature")
+    filter_params = {"filter_by": "key_signature"}
 
+    expected_key_signature = request_params.get("expected_key_signature")
     if not expected_key_signature:
         raise __create_http_error_for_filter_params("key_signature", "expected_key_signature")
 
@@ -404,15 +395,13 @@ def __extract_filter_params_from_request_params_for_key_signature(request_params
 
 
 def __extract_filter_params_from_request_params_for_release_year(request_params):
-    filter_params = {}
+    filter_params = {"filter_by": "release_year"}
 
     min_release_year = __get_request_param_as_int_or_none(request_params, "min_release_year")
-
     if min_release_year is None:
         raise __create_http_error_for_filter_params("release_year", "min_release_year")
 
     max_release_year = __get_request_param_as_int_or_none(request_params, "max_release_year")
-
     if max_release_year is None:
         raise __create_http_error_for_filter_params("release_year", "max_release_year")
 
@@ -422,15 +411,13 @@ def __extract_filter_params_from_request_params_for_release_year(request_params)
 
 
 def __extract_filter_params_from_request_params_for_tempo(request_params):
-    filter_params = {}
+    filter_params = {"filter_by": "tempo"}
 
     min_tempo = __get_request_param_as_int_or_none(request_params, "min_tempo")
-
     if min_tempo is None:
         raise __create_http_error_for_filter_params("tempo", "min_tempo")
 
     max_tempo = __get_request_param_as_int_or_none(request_params, "max_tempo")
-
     if max_tempo is None:
         raise __create_http_error_for_filter_params("tempo", "max_tempo")
 
