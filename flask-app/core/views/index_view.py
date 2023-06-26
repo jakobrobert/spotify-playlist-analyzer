6c25a7825e4e24069e8cc3e56060db2c2553a892
jakobrobert <jakob-robert@hotmail.de>
2023-06-26 20:14:03 +0200

    App: Add "core." prefix to remaining imports & adjust path to config.ini

--- flask-app/core/views/index_view.py
+++ flask-app/core/views/index_view.py
@@ -1,12 +1,10 @@
-# PyCharm shows errors for this import locally, but it works this way with the server
-# 'from app.http_error import HttpError' is shown as valid locally, but does not work with the server
-from http_error import HttpError
+from core.http_error import HttpError
 
 from flask import Blueprint, render_template
 import configparser
 
 config = configparser.ConfigParser()
-config.read("../config.ini")
+config.read("config.ini")
 URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
 
 index_view = Blueprint("index_view", __name__)
