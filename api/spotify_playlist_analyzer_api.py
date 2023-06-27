a5578f0600db4f3723af554bc7a7ec0eba042081
jakobrobert <jakob-robert@hotmail.de>
2023-06-27 20:39:14 +0200

    [App] Rename create_spotify_track to convert_dict_to_track to be less misleading & is consistent to naming in API

--- api/spotify_playlist_analyzer_api.py
+++ api/spotify_playlist_analyzer_api.py
@@ -269,14 +269,13 @@ def search_tracks():
         query = request.args.get("query")
 
         tracks = spotify_client.search_tracks(query)
-
-        tracks_converted = []
+        track_dicts = []
 
         for track in tracks:
             track_dict = __convert_track_to_dict(track)
-            tracks_converted.append(track_dict)
+            track_dicts.append(track_dict)
 
-        return jsonify(tracks_converted)
+        return jsonify(track_dicts)
     except HttpError as error:
         return __create_error_response(error)
     except Exception:
