c63fe1edce13d5a8f678764097bb19906ccf1651
jakobrobert <jakob-robert@hotmail.de>
2023-06-30 09:00:19 +0200

    [API] Move measure_execution_time into new class Utils so can re-use it in multiple files

---
+++ api/core/utils.py
@@ -0,0 +1,21 @@
+import functools
+import time
+
+
+class Utils:
+    @staticmethod
+    def measure_execution_time(log_prefix):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                start_time = time.time()
+                response = func(*args, **kwargs)
+                end_time = time.time()
+                elapsed_time_seconds = end_time - start_time
+                elapsed_time_ms = elapsed_time_seconds * 1000
+                print(f"[Execution time] {log_prefix} {func.__name__} => {elapsed_time_ms} ms")
+                return response
+
+            return wrapper
+
+        return decorator
