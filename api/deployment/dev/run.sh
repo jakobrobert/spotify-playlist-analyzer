1c808c1370a8e82e9467427c1ee1057b51e93748
jakobrobert <jakob-robert@hotmail.de>
2023-07-03 21:05:55 +0200

    [API] Fix deployment scripts

--- api/deployment/dev/run.sh
+++ api/deployment/dev/run.sh
@@ -1,6 +1,6 @@
 source ENV/bin/activate
-export FLASK_APP=spotify_playlist_analyzer:app
+export FLASK_APP=spotify_playlist_analyzer_api:app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=1024
+export FLASK_RUN_PORT=1025
 flask run
