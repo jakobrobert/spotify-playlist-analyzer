3c1dd322756c5652b199434ea762c11a143158ac
jakobrobert <jakob-robert@hotmail.de>
2023-07-19 05:50:10 +0200

    In test_smoke.py, move reading of config file into setUp and move usage of base url into __test_get_request to reduce duplication

--- flask-app/test_smoke.py
+++ flask-app/test_smoke.py
@@ -3,11 +3,6 @@ import unittest
 
 from spotify_playlist_analyzer import app
 
-# TODONOW move into setUp?
-config = configparser.ConfigParser()
-config.read("config.ini")
-URL_PREFIX = config["DEFAULT"]["URL_PREFIX"]
-
 
 class TestSmoke(unittest.TestCase):
     def setUp(self):
@@ -15,34 +10,33 @@ class TestSmoke(unittest.TestCase):
         app.config["DEBUG"] = True
         self.app = app.test_client()
 
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+        self.base_url = config["DEFAULT"]["URL_PREFIX"]
+
     def tearDown(self):
         pass
 
     def test_index(self):
-        url = f"{URL_PREFIX}"
-        self.__test_get_request(url)
+        self.__test_get_request("")
 
     def test_choose_one_playlist(self):
-        url = f"{URL_PREFIX}choose-one-playlist"
-        self.__test_get_request(url)
+        self.__test_get_request("choose-one-playlist")
 
     def test_choose_playlists_for_comparison(self):
-        url = f"{URL_PREFIX}choose-playlists-for-comparison"
-        self.__test_get_request(url)
+        self.__test_get_request("choose-playlists-for-comparison")
 
     def test_choose_one_track(self):
-        url = f"{URL_PREFIX}choose-one-track"
-        self.__test_get_request(url)
+        self.__test_get_request("choose-one-track")
 
     def test_enter_query_to_search_tracks(self):
-        url = f"{URL_PREFIX}enter-query-to-search-tracks"
-        self.__test_get_request(url)
+        self.__test_get_request("enter-query-to-search-tracks")
 
     def test_playlist(self):
-        url = f"{URL_PREFIX}playlist/1v1enByYGutAxxH06UW3cf"
-        self.__test_get_request(url)
+        self.__test_get_request("playlist/1v1enByYGutAxxH06UW3cf")
 
-    def __test_get_request(self, url):
+    def __test_get_request(self, sub_url):
         # TODONOW also move usage of URL_PREFIX into __test_get_request to reduce duplication
+        url = f"{self.base_url}{sub_url}"
         response = self.app.get(url, follow_redirects=True)
         self.assertEqual(response.status_code, 200)
