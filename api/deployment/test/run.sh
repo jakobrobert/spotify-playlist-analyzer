a50d97c665e65831625e9652360aa51b672c3f3b
jakobrobert <jakob-robert@hotmail.de>
2023-07-19 04:43:21 +0200

    Adjust ports, starting from 5000 so can easier remember them

--- api/deployment/test/run.sh
+++ api/deployment/test/run.sh
@@ -2,5 +2,5 @@ source ENV/bin/activate
 export FLASK_APP=spotify_playlist_analyzer_api:app
 export FLASK_ENV=development
 export FLASK_RUN_HOST=0.0.0.0
-export FLASK_RUN_PORT=5003
+export FLASK_RUN_PORT=5005
 flask run
