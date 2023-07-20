e5f15ebdf4e342aa9922d4e8b80bf12c015756db
jakobrobert <jakob-robert@hotmail.de>
2023-07-20 23:01:16 +0200

    Add TODO

--- flask-app/core/views/choose_one_playlist_view.py
+++ flask-app/core/views/choose_one_playlist_view.py
@@ -17,5 +17,6 @@ def choose_one_playlist():
     try:
         return render_template("choose_one_playlist.html")
     except Exception:
+        # TODONOW include status_code in response for all routes, as done in playlist_view.py
         error = HttpError.from_last_exception()
         return render_template("error.html", error=error)
