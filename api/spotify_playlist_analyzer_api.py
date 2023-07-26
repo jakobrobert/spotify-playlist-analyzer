7bb4fcb6820bc126c517590a0a6e8ba037af7c51
jakobrobert <jakob-robert@hotmail.de>
2023-07-26 09:56:28 +0200

    #247 Add attribute distribution for super genres (#258)
    * [API] In endpoint vget_valid_attributes_for_attribute_distribution, add super_genres
    
    * [API] Add super_genres to get_attribute_distribution_items, dummy method in PlaylistStatistics
    
    * [API] Refactor: Define constants in class SuperGenres & list SUPER_GENRES so can iterate through them
    
    * [API] Fix: Returned Metal instead of Others
    
    * [API] Implement get_super_genres_distribution_items
    
    * [API] Implement get_super_genres_distribution_items
    
    * [API] Refactor: Simplify variable names

--- api/spotify_playlist_analyzer_api.py
+++ api/spotify_playlist_analyzer_api.py
@@ -159,7 +159,7 @@ def create_playlist():
 def get_valid_attributes_for_attribute_distribution():
     try:
         attributes = [
-            "duration_ms", "release_year", "popularity",
+            "duration_ms", "release_year", "popularity", "super_genres",
             "tempo", "key", "mode", "key_signature", "loudness",
             "danceability", "energy", "valence", "instrumentalness", "acousticness", "liveness", "speechiness"
         ]
