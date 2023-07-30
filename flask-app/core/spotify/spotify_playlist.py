5c4a64f1ad36dc2dafab278586b2b896c3950559
jakobrobert <jakob-robert@hotmail.de>
2023-07-30 17:49:46 +0200

    Get proper average value

--- flask-app/core/spotify/spotify_playlist.py
+++ flask-app/core/spotify/spotify_playlist.py
@@ -1,4 +1,5 @@
 from core.spotify.spotify_playlist_statistics import SpotifyPlaylistStatistics
+from core.spotify.spotify_track import SpotifyTrack
 
 
 class SpotifyPlaylist:
@@ -13,5 +14,35 @@ class SpotifyPlaylist:
         return f"{percentage:.1f}"
 
     def get_average_value_for_attribute(self, attribute):
-        # TODONOW implement
-        return 42
+        if attribute == "duration_ms":
+            return SpotifyTrack.get_duration_string_helper(self.statistics.average_duration_ms)
+        
+        if attribute == "release_year":
+            return self.statistics.average_release_year
+        
+        if attribute == "popularity":
+            return self.statistics.average_popularity
+        
+        if attribute == "tempo":
+            return self.statistics.average_tempo
+        
+        if attribute == "danceability":
+            return self.statistics.average_danceability
+        
+        if attribute == "energy":
+            return self.statistics.average_energy
+
+        if attribute == "valence":
+            return self.statistics.average_valence
+
+        if attribute == "instrumentalness":
+            return self.statistics.average_instrumentalness
+
+        if attribute == "acousticness":
+            return self.statistics.average_acousticness
+
+        if attribute == "liveness":
+            return self.statistics.average_liveness
+
+        if attribute == "speechiness":
+            return self.statistics.average_speechiness
