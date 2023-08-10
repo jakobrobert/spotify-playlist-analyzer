bd580070caa11836141d7041dd786d368826f903
jakobrobert <jakob-robert@hotmail.de>
2023-08-10 22:09:42 +0200

    [API] Make SpotifyApiAuthorization methods instance based instead of static

--- api/core/spotify/spotify_api_authorization.py
+++ api/core/spotify/spotify_api_authorization.py
@@ -10,12 +10,16 @@ LOG_PREFIX = "SpotifyApiAuthorization."
 
 
 class SpotifyApiAuthorization:
-    @staticmethod
+    def __init__(self, client_id, client_secret, redirect_uri):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+
     @Utils.measure_execution_time(LOG_PREFIX)
-    def get_access_token_by_client_credentials(client_id, client_secret):
+    def get_access_token_by_client_credentials(self):
         url = "https://accounts.spotify.com/api/token"
         data = {"grant_type": "client_credentials"}
-        auth = (client_id, client_secret)
+        auth = (self.client_id, self.client_secret)
         response = requests.post(url, data=data, auth=auth)
         response_data = response.json()
 
@@ -26,17 +30,16 @@ class SpotifyApiAuthorization:
         return response_data["access_token"]
 
     # TODOLATER #171 Fix: Get access token by refresh token fails
-    @staticmethod
     @Utils.measure_execution_time(LOG_PREFIX)
-    def get_access_token_by_refresh_token(self, client_id, client_secret):
+    def get_access_token_by_refresh_token(self, refresh_token):
         url = "https://accounts.spotify.com/api/token"
         headers = {
-            "Authorization": f"Basic {client_id}:{client_secret}",
+            "Authorization": f"Basic {self.client_id}:{self.client_secret}",
             "Content-Type": "application/x-www-form-urlencoded"
         }
         data = {
             "grant_type": "refresh_token",
-            "refresh_token": self.test_refresh_token
+            "refresh_token": refresh_token
         }
 
         response = requests.post(url, headers=headers, data=data)
@@ -50,17 +53,16 @@ class SpotifyApiAuthorization:
 
         return response_data["access_token"]
 
-    @staticmethod
     @Utils.measure_execution_time(LOG_PREFIX)
-    def get_authorization_url(client_id, redirect_uri):
+    def get_authorization_url(self):
         authorization_base_url = "https://accounts.spotify.com/authorize"
         # TODOLATER #171 can use url_for, need to set _external=True
         # First thought that cannot use url_for to get redirect uri because url_for just returned part of the url
         # Therefore hardcoded it in ini file.
         params = {
-            "client_id": client_id,
+            "client_id": self.client_id,
             "response_type": "code",
-            "redirect_uri": redirect_uri,
+            "redirect_uri": self.redirect_uri,
             "scope": "playlist-modify-public"
         }
         params_encoded = urlencode(params)
@@ -69,14 +71,14 @@ class SpotifyApiAuthorization:
         return authorization_url
 
     @Utils.measure_execution_time(LOG_PREFIX)
-    def get_access_and_refresh_token(self, authorization_code, client_id, client_secret, redirect_uri):
+    def get_access_and_refresh_token(self, authorization_code):
         token_url = "https://accounts.spotify.com/api/token"
         data = {
             "grant_type": "authorization_code",
             "code": authorization_code,
-            "client_id": client_id,
-            "client_secret": client_secret,
-            "redirect_uri": redirect_uri,
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "redirect_uri": self.redirect_uri,
         }
 
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
