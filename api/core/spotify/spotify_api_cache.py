1ef490916bd5f3db21006cebf180431cc7ac775f
jakobrobert <jakob-robert@hotmail.de>
2023-08-13 12:41:16 +0200

    Use SpotifyApiCache for SpotifyApiClient.get_playlist_by_id. Works great, but no cache validation yet.

--- api/core/spotify/spotify_api_cache.py
+++ api/core/spotify/spotify_api_cache.py
@@ -12,12 +12,12 @@ class SpotifyApiCache:
 
     def get_playlist_by_id(self, playlist_id):
         if playlist_id not in self.__playlist_by_id_cache:
-            raise KeyError(f"No playlist found in cache for id: {playlist_id}")
+            return None
 
         cache_item = self.__playlist_by_id_cache[playlist_id]
         # TODONOW Invalidate cache
         #   -> check if still up to date, compare timestamp to now. if up to date, return it
-        #   -> else, return exception to indicate that out of date.
+        #   -> else, return None
         return cache_item.data
 
     def update_playlist(self, playlist_id, playlist):
