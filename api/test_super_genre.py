1f3a61b34a6693f4ff8b39e181c4b14b3aed7217
jakobrobert <jakob-robert@hotmail.de>
2023-08-16 09:22:07 +0200

    Add simple unit test

---
+++ api/test_super_genre.py
@@ -0,0 +1,19 @@
+import unittest
+
+from core.spotify.spotify_track import SpotifyTrack
+
+
+class TestSuperGenre(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_pop(self):
+        # TODONOW refactor: Extract util method so do not need to create track
+        track = SpotifyTrack()
+        genres = ["dance pop"]
+        track.update_genres_and_super_genres(genres)
+        expected = ["Pop"]
+        self.assertEqual(expected, track.super_genres)
