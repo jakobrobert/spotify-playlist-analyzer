55ac5253d9a35a34d1f9a2d9332e51d6f13dc99e
jakobrobert <jakob-robert@hotmail.de>
2023-08-16 20:33:54 +0200

    Add more tests

--- api/test_super_genre.py
+++ api/test_super_genre.py
@@ -31,3 +31,30 @@ class TestSuperGenre(unittest.TestCase):
     def test_Rap(self, genre):
         actual = SuperGenreUtils.get_super_genre_for_genre(genre)
         self.assertEqual(SuperGenreUtils.HipHopOrRap, actual)
+
+    @parameterized.expand(["schlager"])
+    def test_Schlager(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Schlager, actual)
+
+    # TODOLATER ExtremeMetal, Metal
+
+    @parameterized.expand(["classical", "opera", "french opera"])
+    def test_Classical(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Classical, actual)
+
+    @parameterized.expand(["classical", "opera", "french opera"])
+    def test_Country(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Classical, actual)
+
+    @parameterized.expand(["afrobeats", "r&b", "soul"])
+    def test_Afro(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Afro, actual)
+
+    @parameterized.expand(["unknown", "garbage", "foobar"])
+    def test_Others(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Others, actual)
