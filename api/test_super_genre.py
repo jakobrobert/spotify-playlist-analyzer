7d23605e5ab81964533b4cb52c372290a71f9d8b
jakobrobert <jakob-robert@hotmail.de>
2023-08-16 20:20:12 +0200

    - Add test for Rap - Use constants of SuperGenreUtils instead of hardcoding strings, to reduce maintenance effort

--- api/test_super_genre.py
+++ api/test_super_genre.py
@@ -15,9 +15,19 @@ class TestSuperGenre(unittest.TestCase):
     @parameterized.expand(["pop", "dance pop", "new romantic"])
     def test_Pop(self, genre):
         actual = SuperGenreUtils.get_super_genre_for_genre(genre)
-        self.assertEqual("Pop", actual)
+        self.assertEqual(SuperGenreUtils.Pop, actual)
+
+    @parameterized.expand(["rock", "hard rock", "mellow gold"])
+    def test_Rock(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.Rock, actual)
 
     @parameterized.expand(["edm", "dance", "house", "eurodance", "german dance"])
     def test_EDM(self, genre):
         actual = SuperGenreUtils.get_super_genre_for_genre(genre)
-        self.assertEqual("EDM", actual)
+        self.assertEqual(SuperGenreUtils.EDM, actual)
+
+    @parameterized.expand(["hip hop", "rap", "german rap"])
+    def test_Rap(self, genre):
+        actual = SuperGenreUtils.get_super_genre_for_genre(genre)
+        self.assertEqual(SuperGenreUtils.HipHopOrRap, actual)
