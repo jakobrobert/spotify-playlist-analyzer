b2363dac0c2f5d275f20ac9229962af03cb723a4
jakobrobert <jakob-robert@hotmail.de>
2023-08-16 08:33:27 +0200

    Change references from PlaylistStatistics to AttributeDistribution in ApiUtils. Now, tests pass

--- api/core/analysis/playlist_statistics.py
+++ api/core/analysis/playlist_statistics.py
@@ -1,10 +1,5 @@
 import statistics
 
-from core.analysis.attribute_distribution_interval import AttributeDistributionInterval
-from core.spotify.spotify_track import SpotifyTrack
-from core.utils import Utils
-
-
 LOG_PREFIX = "PlaylistStatistics."
 
 
