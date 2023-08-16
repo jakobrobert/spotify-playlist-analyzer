6eda1743e359299b798d2f77fafe2246da7fae56
jakobrobert <jakob-robert@hotmail.de>
2023-08-16 20:41:22 +0200

    #290 Add unit tests for assignment of genre to super genre (#295)
    * Add run_tests.sh to API
    
    * Add simple unit test
    
    * Add 2nd test
    
    * Extract class SuperGenreUtils
    
    * Simplify tests
    
    * Add more tests for super genre Pop
    
    * Adjust names of tests
    
    * Add TODO for suggestion how to merge tests
    
    * Merge tests for super genre "Pop" by using parameterized
    
    * Remove the now obsolete tests
    
    * Make EDM test parameterized & add several values for genre
    
    * - Add test for Rap
    - Use constants of SuperGenreUtils instead of hardcoding strings, to reduce maintenance effort
    
    * Add more tests
    
    * Add more tests

---
+++ api/core/analysis/super_genre_utils.py
@@ -0,0 +1,49 @@
+class SuperGenreUtils:
+    Pop = "Pop"
+    Rock = "Rock"
+    EDM = "EDM"
+    HipHopOrRap = "Hip Hop / Rap"
+    Schlager = "Schlager"
+    ExtremeMetal = "Extreme Metal"
+    Metal = "Metal"
+    Classical = "Classical"
+    Country = "Country"
+    Afro = "Afro"
+    Latin = "Latin"
+    Others = "Others"
+    
+    SUPER_GENRES = [
+        Pop, Rock, EDM, HipHopOrRap, Schlager,
+        ExtremeMetal, Metal, Classical, Country,
+        Afro, Latin,
+        Others
+    ]
+
+    ACCEPTED_GENRE_SUBSTRINGS_BY_SUPER_GENRE = {
+        Pop: ["pop", "new romantic", "wave", "girl group", "boy band"],
+        Rock: ["rock", "post-grunge", "punk", "mellow gold"],
+        EDM: [
+            "edm", "dance", "house", "trance", "techno", "hands up", "hardstyle", "big room",
+            "dubstep", "brostep", "complextro", "disco", "hi-nrg", "dancefloor", "drum and bass", "dnb",
+            "jungle", "melbourne bounce"
+        ],
+        HipHopOrRap: ["hip hop", "rap", "trap", "drill"],
+        Schlager: ["schlager"],
+        ExtremeMetal: ["black metal", "death metal", "melodeath"],
+        Metal: ["metal", "neue deutsche harte", "industrial", "screamo", "emo", "nwobhm"],
+        Classical: ["classical", "orchestra", "opera"],
+        Country: ["country"],
+        Afro: ["afro", "r&b", "soul", "reggae", "funk", "urban", "dancehall"],
+        Latin: ["latin"],
+        Others: []
+    }
+
+    @staticmethod
+    def get_super_genre_for_genre(genre):
+        for super_genre in SuperGenreUtils.SUPER_GENRES:
+            accepted_genre_substrings = SuperGenreUtils.ACCEPTED_GENRE_SUBSTRINGS_BY_SUPER_GENRE[super_genre]
+            for accepted_genre_substring in accepted_genre_substrings:
+                if accepted_genre_substring in genre:
+                    return super_genre
+
+        return SuperGenreUtils.Others
