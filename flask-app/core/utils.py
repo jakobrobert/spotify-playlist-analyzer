0a6681456847eb9a2a67e9df7c2d4515ecd7678b
jakobrobert <jakob-robert@hotmail.de>
2023-08-27 17:26:44 +0200

    Fix Error in attribute distribution page for duration if playlist is empty

--- flask-app/core/utils.py
+++ flask-app/core/utils.py
@@ -41,6 +41,9 @@ class Utils:
 
     @staticmethod
     def convert_duration_to_string(duration_ms):
+        if not duration_ms:
+            return "n/a"
+
         duration_ms_int = int(duration_ms)
         total_seconds = duration_ms_int // 1000
         total_minutes = total_seconds // 60
