1fd7b16f14213b94f13882cfdfee845bfd0c2fc8
jakobrobert <jakob-robert@hotmail.de>
2023-09-04 09:29:32 +0200

    Load json from file

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -5,12 +5,10 @@ from test_utils import TestUtils
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        # TODONOW load json from file
-        playlist_json = ""
-        self.top_100_playlist = TestUtils.load_playlist_from_json_string(playlist_json)
-        empty_playlist_json = ""
-        self.empty_playlist = TestUtils.load_playlist_from_json_string(empty_playlist_json)
-        pass
+        self.top_100_playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json")
+        self.empty_playlist = TestUtils.load_playlist_from_json_string(
+            "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json")
 
     def tearDown(self):
         pass
