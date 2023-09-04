692899b89ef4474587289af910f68ab18616effa
jakobrobert <jakob-robert@hotmail.de>
2023-09-04 21:47:28 +0200

    Fully implement load_playlist_from_json_string

--- api/test_utils.py
+++ api/test_utils.py
@@ -9,7 +9,6 @@ class TestUtils:
     def load_playlist_from_json_string(json_string):
         playlist = Playlist()
 
-        # TODONOW FIX fails with JSONDecodeError
         playlist_dict = json.loads(json_string)
         playlist.id = playlist_dict["id"]
         playlist.name = playlist_dict["name"]
@@ -19,7 +18,32 @@ class TestUtils:
         playlist.tracks = []
         for track_dict in tracks_dict:
             track = Track()
-            # TODONOW get all attribute values for Track from dict
+            
+            track.id = track_dict["id"]
+            track.artist_ids = track_dict["artist_ids"]
+            track.artists = track_dict["artists"]
+            track.title = track_dict["title"]
+            track.added_by_user_id = track_dict["added_by_user_id"]
+            track.added_by = track_dict["added_by"]
+            track.duration_ms = track_dict["duration_ms"]
+            track.release_year = track_dict["release_year"]
+            track.popularity = track_dict["popularity"]
+            track.genres = track_dict["genres"]
+            track.super_genres = track_dict["super_genres"]
+            track.tempo = track_dict["tempo"]
+            track.key = track_dict["key"]
+            track.mode = track_dict["mode"]
+            track.key_and_mode_pair = track_dict["key_and_mode_pair"]
+            track.key_signature = track_dict["key_signature"]
+            track.loudness = track_dict["loudness"]
+            track.danceability = track_dict["danceability"]
+            track.energy = track_dict["energy"]
+            track.valence = track_dict["valence"]
+            track.instrumentalness = track_dict["instrumentalness"]
+            track.acousticness = track_dict["acousticness"]
+            track.liveness = track_dict["liveness"]
+            track.speechiness = track_dict["speechiness"]
+            
             playlist.tracks.append(track)
 
         return playlist
