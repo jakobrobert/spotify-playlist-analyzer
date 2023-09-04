4ac16423cf0ebe881e59aedfa7d60011356e2b6b
jakobrobert <jakob-robert@hotmail.de>
2023-09-04 21:30:14 +0200

    Move loading of playlist into tests, out of setUp & tearDown because depending on test, only need one of the 2 playlists

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -4,21 +4,18 @@ from test_utils import TestUtils
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self):
-        self.top_100_playlist = TestUtils.load_playlist_from_json_file(
-            "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json")
-        self.empty_playlist = TestUtils.load_playlist_from_json_file(
-            "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json")
-
-    def tearDown(self):
-        pass
-
     def test_playlist_basic_values(self):
-        self.assertEqual("", self.top_100_playlist.id)
+        playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json")
+
+        self.assertEqual("", playlist.id)
         # TODONOW assert id, name, number of tracks
         pass
 
     def test_empty_playlist_basic_values(self):
+        playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json")
+
         # TODONOW assert id, name, number of tracks
         pass
 
