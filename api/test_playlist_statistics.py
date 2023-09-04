1e4eac893cdb6c4299914a1481b7fa6950acf311
jakobrobert <jakob-robert@hotmail.de>
2023-09-04 21:32:38 +0200

    Implement tests for basic values

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -8,15 +8,16 @@ class MyTestCase(unittest.TestCase):
         playlist = TestUtils.load_playlist_from_json_file(
             "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json")
 
-        self.assertEqual("", playlist.id)
-        # TODONOW assert id, name, number of tracks
-        pass
+        self.assertEqual("6i2Qd6OpeRBAzxfscNXeWp", playlist.id)
+        self.assertEqual("Top 100 Greatest Songs of All Time", playlist.name)
+        self.assertEqual(117, len(playlist.tracks))
 
     def test_empty_playlist_basic_values(self):
         playlist = TestUtils.load_playlist_from_json_file(
             "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json")
 
-        # TODONOW assert id, name, number of tracks
-        pass
+        self.assertEqual("40389fDt9evjBgcgIMAlxe", playlist.id)
+        self.assertEqual("Empty Playlist", playlist.name)
+        self.assertEqual(0, len(playlist.tracks))
 
     # TODONOW add 2 tests for each method of PlaylistStatistics. One for top 100 playlist, one for empty one.
