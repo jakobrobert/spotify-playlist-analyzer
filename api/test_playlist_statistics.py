510120081c67ad9058d87e2a03a5cf0c1c8aa9fe
jakobrobert <jakob-robert@hotmail.de>
2023-09-04 21:28:41 +0200

    Fix loading of json

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
     def setUp(self):
         self.top_100_playlist = TestUtils.load_playlist_from_json_file(
             "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json")
-        self.empty_playlist = TestUtils.load_playlist_from_json_string(
+        self.empty_playlist = TestUtils.load_playlist_from_json_file(
             "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json")
 
     def tearDown(self):
