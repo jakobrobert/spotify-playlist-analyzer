4cbce08b7f6f5fdbb970f74d8624ed9bea9c8973
jakobrobert <jakob-robert@hotmail.de>
2023-09-05 21:24:03 +0200

    #314 Add unit tests in API for PlaylistStatistics (#316)
    * [App] In test_smoke.py, update PLAYLIST_ID_EMPTY so it references empty playlist of test account rather than personal one
    
    * Begin tests, add test data (playlist as json)
    
    * Load json from file
    
    * Try to read basic values from json, but fails with JSONDecodeError
    
    * Fix loading of json
    
    * Move loading of playlist into tests, out of setUp & tearDown because depending on test, only need one of the 2 playlists
    
    * Implement tests for basic values
    
    * Add tests for total duration
    
    * Fully implement load_playlist_from_json_string
    
    * Fix tests.
    
    - For test_normal_playlist_total_duration, value was slightly off. Reasonable, took expected from current playlist, might have changed already since downloaded as json.
    
    - For test_empty_playlist_total_duration, used incorrect path
    
    * Rename test class
    
    * Optimize: Only load playlists once by using setUpClass
    
    * Add tests for average duration
    
    * Use assertIsNone
    
    * Simplify test code by initializing statistics objects in setUpClass
    
    * Add tests for all remaining attributes, garbage expected value
    
    * Put the actual values for the expected ones, now all tests pass

---
+++ api/test_playlist_statistics.py
@@ -0,0 +1,100 @@
+import unittest
+
+from core.analysis.playlist_statistics import PlaylistStatistics
+from test_utils import TestUtils
+
+
+class TestPlaylistStatistics(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.top_100_playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json"
+        )
+        cls.top_100_playlist_statistics = PlaylistStatistics(cls.top_100_playlist.tracks)
+
+        cls.empty_playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json"
+        )
+        cls.empty_playlist_statistics = PlaylistStatistics(cls.empty_playlist.tracks)
+
+    def test_top_100_playlist_basic_values(self):
+        self.assertEqual("6i2Qd6OpeRBAzxfscNXeWp", self.top_100_playlist.id)
+        self.assertEqual("Top 100 Greatest Songs of All Time", self.top_100_playlist.name)
+        self.assertEqual(117, len(self.top_100_playlist.tracks))
+
+    def test_empty_playlist_basic_values(self):
+        self.assertEqual("40389fDt9evjBgcgIMAlxe", self.empty_playlist.id)
+        self.assertEqual("Empty Playlist", self.empty_playlist.name)
+        self.assertEqual(0, len(self.empty_playlist.tracks))
+
+    def test_top_100_playlist_total_duration(self):
+        self.assertEqual(28022939, self.top_100_playlist_statistics.get_total_duration_ms())
+
+    def test_empty_playlist_total_duration(self):
+        self.assertEqual(0, self.empty_playlist_statistics.get_total_duration_ms())
+
+    def test_top_100_playlist_average_duration(self):
+        self.assertAlmostEqual(239512, self.top_100_playlist_statistics.get_average_duration_ms(), 0)
+
+    def test_empty_playlist_average_duration(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_duration_ms())
+
+    def test_top_100_playlist_average_popularity(self):
+        self.assertAlmostEqual(66, self.top_100_playlist_statistics.get_average_popularity(), 0)
+
+    def test_empty_playlist_average_popularity(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_popularity())
+        
+    def test_top_100_playlist_average_release_year(self):
+        self.assertAlmostEqual(1987, self.top_100_playlist_statistics.get_average_release_year(), 0)
+
+    def test_empty_playlist_average_release_year(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_release_year())
+        
+    def test_top_100_playlist_average_tempo(self):
+        self.assertAlmostEqual(118, self.top_100_playlist_statistics.get_average_tempo(), 0)
+
+    def test_empty_playlist_average_tempo(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_tempo())
+        
+    def test_top_100_playlist_average_speechiness(self):
+        self.assertAlmostEqual(5, self.top_100_playlist_statistics.get_average_speechiness(), 0)
+
+    def test_empty_playlist_average_speechiness(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_speechiness())
+        
+    def test_top_100_playlist_average_liveness(self):
+        self.assertAlmostEqual(19, self.top_100_playlist_statistics.get_average_liveness(), 0)
+
+    def test_empty_playlist_average_liveness(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_liveness())
+
+    def test_top_100_playlist_average_acousticness(self):
+        self.assertAlmostEqual(26, self.top_100_playlist_statistics.get_average_acousticness(), 0)
+
+    def test_empty_playlist_average_acousticness(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_acousticness())
+
+    def test_top_100_playlist_average_instrumentalness(self):
+        self.assertAlmostEqual(1, self.top_100_playlist_statistics.get_average_instrumentalness(), 0)
+
+    def test_empty_playlist_average_instrumentalness(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_instrumentalness())
+
+    def test_top_100_playlist_average_valence(self):
+        self.assertAlmostEqual(63, self.top_100_playlist_statistics.get_average_valence(), 0)
+
+    def test_empty_playlist_average_valence(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_valence())
+
+    def test_top_100_playlist_average_energy(self):
+        self.assertAlmostEqual(62, self.top_100_playlist_statistics.get_average_energy(), 0)
+
+    def test_empty_playlist_average_energy(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_energy())
+
+    def test_top_100_playlist_average_danceability(self):
+        self.assertAlmostEqual(58, self.top_100_playlist_statistics.get_average_danceability(), 0)
+
+    def test_empty_playlist_average_danceability(self):
+        self.assertIsNone(self.empty_playlist_statistics.get_average_danceability())
---
+++ api/test_utils.py
@@ -0,0 +1,60 @@
+import json
+
+from core.playlist.playlist import Playlist
+from core.playlist.track import Track
+
+
+class TestUtils:
+    @staticmethod
+    def load_playlist_from_json_string(json_string):
+        playlist = Playlist()
+
+        playlist_dict = json.loads(json_string)
+        playlist.id = playlist_dict["id"]
+        playlist.name = playlist_dict["name"]
+
+        tracks_dict = playlist_dict["tracks"]
+
+        playlist.tracks = []
+        for track_dict in tracks_dict:
+            track = Track()
+            
+            track.id = track_dict["id"]
+            track.artist_ids = track_dict["artist_ids"]
+            track.artists = track_dict["artists"]
+            track.title = track_dict["title"]
+            track.added_by_user_id = track_dict["added_by_user_id"]
+            track.added_by = track_dict["added_by"]
+            track.duration_ms = track_dict["duration_ms"]
+            track.release_year = track_dict["release_year"]
+            track.popularity = track_dict["popularity"]
+            track.genres = track_dict["genres"]
+            track.super_genres = track_dict["super_genres"]
+            track.tempo = track_dict["tempo"]
+            track.key = track_dict["key"]
+            track.mode = track_dict["mode"]
+            track.key_and_mode_pair = track_dict["key_and_mode_pair"]
+            track.key_signature = track_dict["key_signature"]
+            track.loudness = track_dict["loudness"]
+            track.danceability = track_dict["danceability"]
+            track.energy = track_dict["energy"]
+            track.valence = track_dict["valence"]
+            track.instrumentalness = track_dict["instrumentalness"]
+            track.acousticness = track_dict["acousticness"]
+            track.liveness = track_dict["liveness"]
+            track.speechiness = track_dict["speechiness"]
+            
+            playlist.tracks.append(track)
+
+        return playlist
+
+    @staticmethod
+    def load_playlist_from_json_file(file_path):
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                json_string = file.read()
+
+            return TestUtils.load_playlist_from_json_string(json_string)
+        except FileNotFoundError as e:
+            print(f"load_playlist_from_json_file failed: file_path: '{file_path}', e: {e}")
+
