07d71479c9c5359df65f55659bb2c6215180e77c
jakobrobert <jakob-robert@hotmail.de>
2023-09-05 20:59:17 +0200

    Optimize: Only load playlists once by using setUpClass

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -3,34 +3,34 @@ import unittest
 from core.analysis.playlist_statistics import PlaylistStatistics
 from test_utils import TestUtils
 
-NORMAL_PLAYLIST_FILE_PATH = "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json"
-EMPTY_PLAYLIST_FILE_PATH = "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json"
-
 
 class TestPlaylistStatistics(unittest.TestCase):
-    def test_normal_playlist_basic_values(self):
-        playlist = TestUtils.load_playlist_from_json_file(NORMAL_PLAYLIST_FILE_PATH)
+    @classmethod
+    def setUpClass(cls):
+        cls.top_100_playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json"
+        )
+        cls.empty_playlist = TestUtils.load_playlist_from_json_file(
+            "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json"
+        )
 
-        self.assertEqual("6i2Qd6OpeRBAzxfscNXeWp", playlist.id)
-        self.assertEqual("Top 100 Greatest Songs of All Time", playlist.name)
-        self.assertEqual(117, len(playlist.tracks))
+    def test_top_100_playlist_basic_values(self):
+        self.assertEqual("6i2Qd6OpeRBAzxfscNXeWp", self.top_100_playlist.id)
+        self.assertEqual("Top 100 Greatest Songs of All Time", self.top_100_playlist.name)
+        self.assertEqual(117, len(self.top_100_playlist.tracks))
 
     def test_empty_playlist_basic_values(self):
-        playlist = TestUtils.load_playlist_from_json_file(EMPTY_PLAYLIST_FILE_PATH)
-
-        self.assertEqual("40389fDt9evjBgcgIMAlxe", playlist.id)
-        self.assertEqual("Empty Playlist", playlist.name)
-        self.assertEqual(0, len(playlist.tracks))
+        self.assertEqual("40389fDt9evjBgcgIMAlxe", self.empty_playlist.id)
+        self.assertEqual("Empty Playlist", self.empty_playlist.name)
+        self.assertEqual(0, len(self.empty_playlist.tracks))
 
     # TODONOW add 2 tests for each method of PlaylistStatistics. One for top 100 playlist, one for empty one.
-    def test_normal_playlist_total_duration(self):
-        playlist = TestUtils.load_playlist_from_json_file(NORMAL_PLAYLIST_FILE_PATH)
-        statistics = PlaylistStatistics(playlist.tracks)
+    def test_top_100_playlist_total_duration(self):
+        statistics = PlaylistStatistics(self.top_100_playlist.tracks)
 
         self.assertEqual(28022939, statistics.get_total_duration_ms())
 
     def test_empty_playlist_total_duration(self):
-        playlist = TestUtils.load_playlist_from_json_file(EMPTY_PLAYLIST_FILE_PATH)
-        statistics = PlaylistStatistics(playlist.tracks)
+        statistics = PlaylistStatistics(self.empty_playlist.tracks)
 
         self.assertEqual(0, statistics.get_total_duration_ms())
