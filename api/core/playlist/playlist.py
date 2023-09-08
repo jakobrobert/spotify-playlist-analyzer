4470e8f770720d34e45f76d986dba459da7d86c7
jakobrobert <jakob-robert@hotmail.de>
2023-09-08 09:39:25 +0200

    #318 [API] Refactor TestUtils & production code for dict stuff (#321)
    * Remove exception handling
    
    * In TestUtils, extract methods
    
    * Adjust __create_playlist_from_dict
    
    * Remove load_playlist_from_json_string, no need to load from string directly
    
    * Add method from_dict to Playlist & Track
    
    * Use Playlist.from_dict in TestUtils
    
    * Extract __playlist_to_dict
    
    * Explicitly copy track dict
    
    * Move to_dict into Playlist class

--- api/core/playlist/playlist.py
+++ api/core/playlist/playlist.py
@@ -1,5 +1,36 @@
+from core.playlist.track import Track
+
+
 class Playlist:
     def __init__(self):
         self.id = "n/a"
         self.name = "n/a"
         self.tracks = []
+
+    def to_dict(self):
+        # Need to explicitly copy the dict, else changing the dict would change the original object
+        playlist_dict = dict(self.__dict__)
+
+        # Need to convert tracks to dict manually, playlist.__dict__ does not work recursively
+        playlist_dict["tracks"] = []
+        for track in self.tracks:
+            # Explicitly copy dict for consistency and robustness
+            track_dict = dict(track.__dict__)
+            playlist_dict["tracks"].append(track_dict)
+
+        return playlist_dict
+
+    @staticmethod
+    def from_dict(playlist_dict):
+        playlist = Playlist()
+
+        playlist.id = playlist_dict["id"]
+        playlist.name = playlist_dict["name"]
+        tracks_dict = playlist_dict["tracks"]
+        playlist.tracks = []
+
+        for track_dict in tracks_dict:
+            track = Track.from_dict(track_dict)
+            playlist.tracks.append(track)
+
+        return playlist
