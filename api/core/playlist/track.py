3835395aa9195b8eb484cfd4da6048f8d7a6ba9a
jakobrobert <jakob-robert@hotmail.de>
2023-09-08 09:26:38 +0200

    Add method from_dict to Playlist & Track

--- api/core/playlist/track.py
+++ api/core/playlist/track.py
@@ -105,6 +105,37 @@ class Track:
         self.liveness = Track.__process_audio_feature_value(audio_features["liveness"])
         self.speechiness = Track.__process_audio_feature_value(audio_features["speechiness"])
 
+    @staticmethod
+    def from_dict(track_dict):
+        track = Track()
+
+        track.id = track_dict["id"]
+        track.artist_ids = track_dict["artist_ids"]
+        track.artists = track_dict["artists"]
+        track.title = track_dict["title"]
+        track.added_by_user_id = track_dict["added_by_user_id"]
+        track.added_by = track_dict["added_by"]
+        track.duration_ms = track_dict["duration_ms"]
+        track.release_year = track_dict["release_year"]
+        track.popularity = track_dict["popularity"]
+        track.genres = track_dict["genres"]
+        track.super_genres = track_dict["super_genres"]
+        track.tempo = track_dict["tempo"]
+        track.key = track_dict["key"]
+        track.mode = track_dict["mode"]
+        track.key_and_mode_pair = track_dict["key_and_mode_pair"]
+        track.key_signature = track_dict["key_signature"]
+        track.loudness = track_dict["loudness"]
+        track.danceability = track_dict["danceability"]
+        track.energy = track_dict["energy"]
+        track.valence = track_dict["valence"]
+        track.instrumentalness = track_dict["instrumentalness"]
+        track.acousticness = track_dict["acousticness"]
+        track.liveness = track_dict["liveness"]
+        track.speechiness = track_dict["speechiness"]
+
+        return track
+
     def __update_super_genres_by_genres(self, genres):
         super_genres_for_this_track = []
         for genre in genres:
