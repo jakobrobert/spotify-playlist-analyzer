c0483151db5e985e0211b17b33083ae3bda2bf4e
jakobrobert <jakob-robert@hotmail.de>
2023-09-08 09:01:14 +0200

    Remove exception handling

--- api/test_utils.py
+++ api/test_utils.py
@@ -50,11 +50,8 @@ class TestUtils:
 
     @staticmethod
     def load_playlist_from_json_file(file_path):
-        try:
-            with open(file_path, "r", encoding="utf-8") as file:
-                json_string = file.read()
+        with open(file_path, "r", encoding="utf-8") as file:
+            json_string = file.read()
 
-            return TestUtils.load_playlist_from_json_string(json_string)
-        except FileNotFoundError as e:
-            print(f"load_playlist_from_json_file failed: file_path: '{file_path}', e: {e}")
+        return TestUtils.load_playlist_from_json_string(json_string)
 
