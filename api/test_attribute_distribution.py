b6478ae437c20ba0fe538ff5007e831802f2509c
jakobrobert <jakob-robert@hotmail.de>
2023-09-11 11:56:35 +0200

    Continue test a bit, but now decided to load expected values from json, would be too much work and not maintainable to hardcode everything

--- api/test_attribute_distribution.py
+++ api/test_attribute_distribution.py
@@ -22,16 +22,40 @@ class TestAttributeDistribution(unittest.TestCase):
 
         self.assertEqual(8, len(duration_distribution_items))
 
-        # TODONOW test "count" for each item
-        # TODONOW test "percentage" for each item
         self.assertEqual("< 02:00", duration_distribution_items[0]["label"])
+        self.assertEqual(1, duration_distribution_items[0]["count"])
+        self.assertAlmostEqual(0.9, duration_distribution_items[0]["percentage"], 1)
+
         self.assertEqual("≥ 02:00", duration_distribution_items[1]["label"])
+        self.assertEqual(12, duration_distribution_items[1]["count"])
+        self.assertAlmostEqual(10.3, duration_distribution_items[1]["percentage"], 1)
+
+        # TODONOW better load expected values from json, would be too much work and not maintainable to hardcode everything
+        """
         self.assertEqual("≥ 02:30", duration_distribution_items[2]["label"])
+        self.assertEqual(1, duration_distribution_items[2]["count"])
+        self.assertIsNone(duration_distribution_items[2]["percentage"])
+
         self.assertEqual("≥ 03:00", duration_distribution_items[3]["label"])
+        self.assertEqual(1, duration_distribution_items[3]["count"])
+        self.assertIsNone(duration_distribution_items[3]["percentage"])
+
         self.assertEqual("≥ 03:30", duration_distribution_items[4]["label"])
+        self.assertEqual(1, duration_distribution_items[4]["count"])
+        self.assertIsNone(duration_distribution_items[4]["percentage"])
+
         self.assertEqual("≥ 04:00", duration_distribution_items[5]["label"])
+        self.assertEqual(1, duration_distribution_items[5]["count"])
+        self.assertIsNone(duration_distribution_items[5]["percentage"])
+
         self.assertEqual("≥ 04:30", duration_distribution_items[6]["label"])
+        self.assertEqual(1, duration_distribution_items[6]["count"])
+        self.assertIsNone(duration_distribution_items[6]["percentage"])
+
         self.assertEqual("≥ 05:00", duration_distribution_items[7]["label"])
+        self.assertEqual(1, duration_distribution_items[7]["count"])
+        self.assertIsNone(duration_distribution_items[7]["percentage"])
+        """
 
     @classmethod
     def __load__and_validate_top_100_playlist(cls):
