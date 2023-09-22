2c6e9bd36bfd7d06c25d9b10e8f526eb292ba6bf
jakobrobert <jakob-robert@hotmail.de>
2023-09-22 15:23:14 +0200

    Extract duplicated code into TestUtils

--- api/test_attribute_distribution.py
+++ api/test_attribute_distribution.py
@@ -10,10 +10,10 @@ from test_utils import TestUtils
 class TestAttributeDistribution(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        top_100_playlist = cls.__load__and_validate_top_100_playlist()
+        top_100_playlist = TestUtils.load__and_validate_top_100_playlist()
         cls.top_100_playlist_attribute_distribution = AttributeDistribution(top_100_playlist.tracks)
 
-        empty_playlist = cls.__load_and_validate_empty_playlist()
+        empty_playlist = TestUtils.load_and_validate_empty_playlist()
         cls.empty_playlist_attribute_distribution = AttributeDistribution(empty_playlist.tracks)
 
     def test_top_100_playlist_duration_distribution(self):
@@ -185,34 +185,6 @@ class TestAttributeDistribution(unittest.TestCase):
         expected_items = self.__load_expected_distribution_items_for_empty_playlist("valence")
         actual_items = self.empty_playlist_attribute_distribution.get_valence_items()
         self.__assert_distribution_items(actual_items, expected_items)
-
-    @staticmethod
-    def __load__and_validate_top_100_playlist():
-        playlist_id = "26LDpXWgS0nYibyLS9X4Wq"
-        playlist_directory_name = f"{playlist_id}_top_100_greatest_songs_of_all_time"
-        file_path = f"test_data/playlists/{playlist_directory_name}/playlist.json"
-        playlist = TestUtils.load_playlist_from_json_file(file_path)
-
-        # We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "26LDpXWgS0nYibyLS9X4Wq" == playlist.id
-        assert "Top 100 Greatest Songs of All Time [Backup on 2023-09-15 14:40 UTC]" == playlist.name
-        assert 117 == len(playlist.tracks)
-
-        return playlist
-
-    @staticmethod
-    def __load_and_validate_empty_playlist():
-        playlist_id = "40389fDt9evjBgcgIMAlxe"
-        playlist_directory_name = f"{playlist_id}_empty"
-        file_path = f"test_data/playlists/{playlist_directory_name}/playlist.json"
-        playlist = TestUtils.load_playlist_from_json_file(file_path)
-
-        # We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "40389fDt9evjBgcgIMAlxe" == playlist.id
-        assert "Empty Playlist" == playlist.name
-        assert 0 == len(playlist.tracks)
-
-        return playlist
 
     @staticmethod
     def __load_expected_distribution_items(playlist_directory_name, attribute_name):
