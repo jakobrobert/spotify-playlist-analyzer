092ad213e2f3837fed8a2e7963775bd70421da63
jakobrobert <jakob-robert@hotmail.de>
2023-09-22 15:29:26 +0200

    Add constants for playlist ids to TestUtils

--- api/test_utils.py
+++ api/test_utils.py
@@ -4,6 +4,9 @@ from core.playlist.playlist import Playlist
 
 
 class TestUtils:
+    PLAYLIST_ID_TOP_100 = "26LDpXWgS0nYibyLS9X4Wq"
+    PLAYLIST_ID_EMPTY = "40389fDt9evjBgcgIMAlxe"
+
     @staticmethod
     def load_playlist_from_json_file(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
@@ -13,13 +16,12 @@ class TestUtils:
 
     @staticmethod
     def load__and_validate_top_100_playlist():
-        playlist_id = "26LDpXWgS0nYibyLS9X4Wq"
-        playlist_directory_name = f"{playlist_id}_top_100_greatest_songs_of_all_time"
+        playlist_directory_name = f"{TestUtils.PLAYLIST_ID_TOP_100}_top_100_greatest_songs_of_all_time"
         file_path = f"test_data/playlists/{playlist_directory_name}/playlist.json"
         playlist = TestUtils.load_playlist_from_json_file(file_path)
 
         # We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "26LDpXWgS0nYibyLS9X4Wq" == playlist.id
+        assert TestUtils.PLAYLIST_ID_TOP_100 == playlist.id
         assert "Top 100 Greatest Songs of All Time [Backup on 2023-09-15 14:40 UTC]" == playlist.name
         assert 117 == len(playlist.tracks)
 
@@ -27,13 +29,12 @@ class TestUtils:
 
     @staticmethod
     def load_and_validate_empty_playlist():
-        playlist_id = "40389fDt9evjBgcgIMAlxe"
-        playlist_directory_name = f"{playlist_id}_empty"
+        playlist_directory_name = f"{TestUtils.PLAYLIST_ID_EMPTY}_empty"
         file_path = f"test_data/playlists/{playlist_directory_name}/playlist.json"
         playlist = TestUtils.load_playlist_from_json_file(file_path)
 
         # We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "40389fDt9evjBgcgIMAlxe" == playlist.id
+        assert TestUtils.PLAYLIST_ID_EMPTY == playlist.id
         assert "Empty Playlist" == playlist.name
         assert 0 == len(playlist.tracks)
 
