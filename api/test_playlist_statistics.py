14a47d1e3424c16433412b831fc2fc4b5fe730b9
jakobrobert <jakob-robert@hotmail.de>
2023-09-22 16:24:59 +0200

    #243 [API] Add unit tests for AttributeDistribution (#322)
    * Begin with tests, load playlists in setUpClass
    
    * In test_top_100_playlist_duration_distribution, add asserts for labels
    
    * Continue test a bit, but now decided to load expected values from json, would be too much work and not maintainable to hardcode everything
    
    * In test_data, create sub directory playlists
    
    * Add test_data json files for expected values of attribute distribution
    
    * Reformat json files
    
    * Remove hardcoded asserts & adjust TODOs
    
    * Create backup for top 100 playlist and update json files. This way, playlist is stable, original one might change and then might get inconsistent data
    
    * Fix tests due to update of playlist data
    
    * Make classmethod to staticmethod
    
    * Load expected_duration_distribution_items from json file
    
    * In tests for duration distribution, add assertions dynamically based on loaded data
    
    * Simplify variable names
    
    * Preparatory refactoring
    
    * Fix error introduced on purpose, tests pass again
    
    * Extract method __load_expected_attribute_distribution_items, tests pass
    
    * Extract method __assert_distribution_items, tests pass
    
    * Add trailing newline
    
    * Add tests for release_year. Fail as expected because files do not exist.
    
    * Add test data for release_year. But tests fail: AssertionError: 6 != 8
    
    * Fix tests. Just called wrong method
    
    * Extract wrapper methods to load expected distribution items, to avoid duplicated playlist id
    
    * Small refactoring in load_and_validate...
    
    * Renaming
    
    * Remove empty lines
    
    * Add tests for all remaining attributes. They fail as expected because test data does not exist
    
    * Remove obsolete empty lines. Before, added them to separate sections of Arrange-Act-Assert, but by refactoring the code is short enough
    
    * Adjust directory structure of test_data
    
    * Extract duplicated code into TestUtils
    
    * Add constants for playlist ids to TestUtils
    
    * Add constants for directory name. Comment out unfinished tests, other ones pass as expected
    
    * Add test data for top 100 playlist, for remaining attributes
    
    * Enable tests & add missing test data for energy. Now, all tests for top 100 playlist pass.
    
    * Add remaining test data. Now all tests pass EXCEPT test_top_100_playlist_popularity_distribution - AssertionError: 7 != 9
    
    * Comment out test_top_100_playlist_popularity_distribution & add todo comment
    
    * Reformat test data json files

--- api/test_playlist_statistics.py
+++ api/test_playlist_statistics.py
@@ -10,26 +10,26 @@ from test_utils import TestUtils
 class TestPlaylistStatistics(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        top_100_playlist = cls.__load__and_validate_top_100_playlist()
+        top_100_playlist = TestUtils.load__and_validate_top_100_playlist()
         cls.top_100_playlist_statistics = PlaylistStatistics(top_100_playlist.tracks)
 
-        empty_playlist = cls.__load_and_validate_empty_playlist()
+        empty_playlist = TestUtils.load_and_validate_empty_playlist()
         cls.empty_playlist_statistics = PlaylistStatistics(empty_playlist.tracks)
 
     def test_top_100_playlist_total_duration(self):
-        self.assertEqual(28022939, self.top_100_playlist_statistics.get_total_duration_ms())
+        self.assertEqual(28014935, self.top_100_playlist_statistics.get_total_duration_ms())
 
     def test_empty_playlist_total_duration(self):
         self.assertEqual(0, self.empty_playlist_statistics.get_total_duration_ms())
 
     def test_top_100_playlist_average_duration(self):
-        self.assertAlmostEqual(239512, self.top_100_playlist_statistics.get_average_duration_ms(), 0)
+        self.assertAlmostEqual(239444, self.top_100_playlist_statistics.get_average_duration_ms(), 0)
 
     def test_empty_playlist_average_duration(self):
         self.assertIsNone(self.empty_playlist_statistics.get_average_duration_ms())
 
     def test_top_100_playlist_average_popularity(self):
-        self.assertAlmostEqual(66, self.top_100_playlist_statistics.get_average_popularity(), 0)
+        self.assertAlmostEqual(65, self.top_100_playlist_statistics.get_average_popularity(), 0)
 
     def test_empty_playlist_average_popularity(self):
         self.assertIsNone(self.empty_playlist_statistics.get_average_popularity())
@@ -59,7 +59,7 @@ class TestPlaylistStatistics(unittest.TestCase):
         self.assertIsNone(self.empty_playlist_statistics.get_average_liveness())
 
     def test_top_100_playlist_average_acousticness(self):
-        self.assertAlmostEqual(26, self.top_100_playlist_statistics.get_average_acousticness(), 0)
+        self.assertAlmostEqual(27, self.top_100_playlist_statistics.get_average_acousticness(), 0)
 
     def test_empty_playlist_average_acousticness(self):
         self.assertIsNone(self.empty_playlist_statistics.get_average_acousticness())
@@ -71,7 +71,7 @@ class TestPlaylistStatistics(unittest.TestCase):
         self.assertIsNone(self.empty_playlist_statistics.get_average_instrumentalness())
 
     def test_top_100_playlist_average_valence(self):
-        self.assertAlmostEqual(63, self.top_100_playlist_statistics.get_average_valence(), 0)
+        self.assertAlmostEqual(62, self.top_100_playlist_statistics.get_average_valence(), 0)
 
     def test_empty_playlist_average_valence(self):
         self.assertIsNone(self.empty_playlist_statistics.get_average_valence())
@@ -87,25 +87,3 @@ class TestPlaylistStatistics(unittest.TestCase):
 
     def test_empty_playlist_average_danceability(self):
         self.assertIsNone(self.empty_playlist_statistics.get_average_danceability())
-
-    @classmethod
-    def __load__and_validate_top_100_playlist(cls):
-        path = "./test_data/playlist_6i2Qd6OpeRBAzxfscNXeWp_top_100_greatest_songs_of_all_time.json"
-        top_100_playlist = TestUtils.load_playlist_from_json_file(path)
-
-        # Note: We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "6i2Qd6OpeRBAzxfscNXeWp" == top_100_playlist.id
-        assert "Top 100 Greatest Songs of All Time" == top_100_playlist.name
-        assert 117 == len(top_100_playlist.tracks)
-        return top_100_playlist
-
-    @classmethod
-    def __load_and_validate_empty_playlist(cls):
-        path = "./test_data/playlist_40389fDt9evjBgcgIMAlxe_empty.json"
-        empty_playlist = TestUtils.load_playlist_from_json_file(path)
-
-        # Note: We use assert instead of assertEquals because cannot use it in setUpClass
-        assert "40389fDt9evjBgcgIMAlxe" == empty_playlist.id
-        assert "Empty Playlist" == empty_playlist.name
-        assert 0 == len(empty_playlist.tracks)
-        return empty_playlist
--- api/test_utils.py
+++ api/test_utils.py
@@ -4,9 +4,39 @@ from core.playlist.playlist import Playlist
 
 
 class TestUtils:
+    TOP_100_PLAYLIST_ID = "26LDpXWgS0nYibyLS9X4Wq"
+    TOP_100_PLAYLIST_DIRECTORY_NAME = f"{TOP_100_PLAYLIST_ID}_top_100_greatest_songs_of_all_time"
+
+    EMPTY_PLAYLIST_ID = "40389fDt9evjBgcgIMAlxe"
+    EMPTY_PLAYLIST_DIRECTORY_NAME = f"{EMPTY_PLAYLIST_ID}_empty"
+
     @staticmethod
     def load_playlist_from_json_file(file_path):
         with open(file_path, "r", encoding="utf-8") as file:
             playlist_dict = json.load(file)
 
         return Playlist.from_dict(playlist_dict)
+
+    @staticmethod
+    def load__and_validate_top_100_playlist():
+        file_path = f"test_data/playlists/{TestUtils.TOP_100_PLAYLIST_DIRECTORY_NAME}/playlist.json"
+        playlist = TestUtils.load_playlist_from_json_file(file_path)
+
+        # We use assert instead of assertEquals because cannot use it in setUpClass
+        assert TestUtils.TOP_100_PLAYLIST_ID == playlist.id
+        assert "Top 100 Greatest Songs of All Time [Backup on 2023-09-15 14:40 UTC]" == playlist.name
+        assert 117 == len(playlist.tracks)
+
+        return playlist
+
+    @staticmethod
+    def load_and_validate_empty_playlist():
+        file_path = f"test_data/playlists/{TestUtils.EMPTY_PLAYLIST_DIRECTORY_NAME}/playlist.json"
+        playlist = TestUtils.load_playlist_from_json_file(file_path)
+
+        # We use assert instead of assertEquals because cannot use it in setUpClass
+        assert TestUtils.EMPTY_PLAYLIST_ID == playlist.id
+        assert "Empty Playlist" == playlist.name
+        assert 0 == len(playlist.tracks)
+
+        return playlist
