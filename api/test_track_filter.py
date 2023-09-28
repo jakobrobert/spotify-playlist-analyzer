6b3d8f2ef902b5ea6d97e83a07053b8aa73b957c
jakobrobert <jakob-robert@hotmail.de>
2023-09-28 08:27:54 +0200

    Add ids for tracks to get more meaningful fail message. Just got memory addresses of Objects before

--- api/test_track_filter.py
+++ api/test_track_filter.py
@@ -12,15 +12,19 @@ class TestTrackFilter(unittest.TestCase):
             "max_tempo": 120
         }
         accepted_track = Track()
+        accepted_track.id = "accepted"
         accepted_track.tempo = 110
         not_accepted_track_1 = Track()
+        not_accepted_track_1.id = "not accepted 1"
         not_accepted_track_1.tempo = 90
         not_accepted_track_2 = Track()
+        not_accepted_track_2.id = "not accepted 2"
         not_accepted_track_2.tempo = 130
         tracks = [accepted_track, not_accepted_track_1, not_accepted_track_2]
-        expected_filtered_tracks = [accepted_track]
+        expected_filtered_track_ids = ["accepted", "not accepted 1"] # TODONOW fix, error on purpose
 
         track_filter = TrackFilter(tracks, filter_params)
         actual_filtered_tracks = track_filter.filter_tracks()
+        actual_filtered_track_ids = [track.id for track in actual_filtered_tracks]
 
-        self.assertEqual(expected_filtered_tracks, actual_filtered_tracks)
+        self.assertEqual(expected_filtered_track_ids, actual_filtered_track_ids)
