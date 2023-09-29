737aa26fa137fda446408b9ea20b4b5f835ff865
jakobrobert <jakob-robert@hotmail.de>
2023-09-29 08:31:52 +0200

    Refactor duplicated code by combining tests into one function using parameterized

--- api/test_track_filter.py
+++ api/test_track_filter.py
@@ -1,58 +1,29 @@
 import unittest
 
+from parameterized import parameterized
+
 from core.playlist.track import Track
 from core.track_filter import TrackFilter
 
 
 class TestTrackFilter(unittest.TestCase):
-    # TODONOW refactor duplicated code
-    def test_filter_by_tempo_accepted(self):
+    @parameterized.expand([
+        [110, True],
+        [90, False],
+        [130, False]
+    ])
+    def test_filter_by_tempo(self, tempo, accepted):
         filter_params = {
             "filter_by": "tempo",
             "min_tempo": 100,
             "max_tempo": 120
         }
-
-        accepted_track = Track()
-        accepted_track.tempo = 110
-        tracks = [accepted_track]
-        expected_filtered_tracks = [accepted_track]
-
-        track_filter = TrackFilter(tracks, filter_params)
-        actual_filtered_tracks = track_filter.filter_tracks()
-
-        self.assertEqual(expected_filtered_tracks, actual_filtered_tracks)
-
-    def test_filter_by_tempo_not_accepted_because_too_small(self):
-        filter_params = {
-            "filter_by": "tempo",
-            "min_tempo": 100,
-            "max_tempo": 120
-        }
-
-        accepted_track = Track()
-        accepted_track.tempo = 90
-        tracks = [accepted_track]
+        track = Track()
+        track.tempo = tempo
+        tracks = [track]
+        expected_filtered_tracks = [track] if accepted else []
 
         track_filter = TrackFilter(tracks, filter_params)
         actual_filtered_tracks = track_filter.filter_tracks()
-        expected_filtered_tracks = []
-
-        self.assertEqual(expected_filtered_tracks, actual_filtered_tracks)
-
-    def test_filter_by_tempo_not_accepted_because_too_big(self):
-        filter_params = {
-            "filter_by": "tempo",
-            "min_tempo": 100,
-            "max_tempo": 120
-        }
-
-        accepted_track = Track()
-        accepted_track.tempo = 130
-        tracks = [accepted_track]
-
-        track_filter = TrackFilter(tracks, filter_params)
-        actual_filtered_tracks = track_filter.filter_tracks()
-        expected_filtered_tracks = []
 
         self.assertEqual(expected_filtered_tracks, actual_filtered_tracks)
