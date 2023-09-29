08fa93824d95666ddbb817ebf1b090cf5fed01ea
jakobrobert <jakob-robert@hotmail.de>
2023-09-29 08:38:43 +0200

    Add test_filter_by_release_year

--- api/test_track_filter.py
+++ api/test_track_filter.py
@@ -8,6 +8,28 @@ from core.track_filter import TrackFilter
 
 class TestTrackFilter(unittest.TestCase):
     @parameterized.expand([
+        [1985, True],
+        [1970, False],
+        [1990, False]
+    ])
+    def test_filter_by_release_year(self, release_year, accepted):
+        filter_params = {
+            "filter_by": "release_year",
+            "min_release_year": 1980,
+            "max_release_year": 1989
+        }
+        track = Track()
+        track.release_year = release_year
+        tracks = [track]
+        # TODONOW just assert length, there is only one track anyway.
+        expected_filtered_tracks = [track] if accepted else []
+
+        track_filter = TrackFilter(tracks, filter_params)
+        actual_filtered_tracks = track_filter.filter_tracks()
+
+        self.assertEqual(expected_filtered_tracks, actual_filtered_tracks)
+
+    @parameterized.expand([
         [110, True],
         [90, False],
         [130, False]
