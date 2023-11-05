9d4aedf32131d83c6d3fe8f9620038979b742cc5
jakobrobert <jakob-robert@hotmail.de>
2023-11-05 13:07:48 +0100

    Add tests for track url

--- flask-app/test_view_utils.py
+++ flask-app/test_view_utils.py
@@ -24,9 +24,25 @@ class TestSmoke(unittest.TestCase):
         self.assertEqual(expected_playlist_id, actual_playlist_id)
 
     @parameterized.expand([
+        ["foo"],
         ["https://open.spotify.com/intl-de/track/5XcZRgJv3zMhTqCyESjQrF?si=0815a93806cd484b"],
-        ["foo"]
     ])
     def test_get_playlist_id_from_playlist_url_invalid(self, playlist_url):
         with self.assertRaises(Exception):
             ViewUtils.get_playlist_id_from_playlist_url(playlist_url)
+
+    @parameterized.expand([
+        ["https://open.spotify.com/intl-de/track/5XcZRgJv3zMhTqCyESjQrF?si=0815a93806cd484b", "5XcZRgJv3zMhTqCyESjQrF"],
+        ["https://open.spotify.com/intl-de/track/5XcZRgJv3zMhTqCyESjQrF", "5XcZRgJv3zMhTqCyESjQrF"],
+    ])
+    def test_get_track_id_from_track_url_valid(self, track_url, expected_track_id):
+        actual_track_id = ViewUtils.get_track_id_from_track_url(track_url)
+        self.assertEqual(expected_track_id, actual_track_id)
+
+    @parameterized.expand([
+        ["foo"],
+        ["https://open.spotify.com/playlist/4PEUZYZ3RoKEcZQoGqu5mV?si=0c08f72180474249"],
+    ])
+    def test_get_track_id_from_track_url_invalid(self, track_url):
+        with self.assertRaises(Exception):
+            ViewUtils.get_track_id_from_track_url(track_url)
